d2e187a3358a1e39f571e9f376926ff99913375b
Your Name <braught@dickinson.edu>
2023-11-06 09:32:07 -0500

    checkpoint

--- bin/addComponent.bash
+++ bin/addComponent.bash
@@ -10,15 +10,6 @@ SCRIPT_PATH=$(readlink -f "$0")                     # Path to this script.
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")                # Path to directory containing this script.
 REPO_ROOT_DIR=$(builtin cd "$SCRIPT_DIR/.." && pwd) # REPO root directory.
 
-# Check that the development branch is checked out
-CUR_GIT_BRANCH=$(git branch)
-if [[ ! "$CUR_GIT_BRANCH" == *"* development"* ]]; then
-  echo -e "${ON_RED}ERROR:${NO_COLOR} You must have the development branch checked out to add an entry point."
-  echo "Switch to the development branch."
-  echo "Then run this script again."
-  exit 255
-fi
-
 # Check that working tree is clean
 GIT_STATUS=$(git status | tail -1)
 if [[ ! "$GIT_STATUS" =~ ^"nothing to commit, working tree clean"$ ]]; then
@@ -38,7 +29,7 @@ echo ""
 
 FEATURE_BRANCH_NAME="add_$COMPONENT_NAME""_component"
 
-# Check if a feature branch already exists for the comonent...
+# Check if a feature branch already exists for the component...
 FEATURE_BRANCH_EXISTS=$(git branch -a | grep "$FEATURE_BRANCH_NAME")
 if [[ ! "$FEATURE_BRANCH_EXISTS" == "" ]]; then
   echo -e "${ON_RED}ERROR:${NO_COLOR} The feature branch $FEATURE_BRANCH_NAME already exists."
@@ -48,8 +39,51 @@ if [[ ! "$FEATURE_BRANCH_EXISTS" == "" ]]; then
   exit 255
 fi
 
-# Create a new feature branch for the component.
-git branch "$FEATURE_BRANCH_NAME"
-echo "Created feature branch '$FEATURE_BRANCH_NAME"
-git switch "$FEATURE_BRANCH_NAME"
+echo "About to add a component as follows:"
+echo "       component directory: $COMPONENTS_DIR"
+echo "               component name: $COMPONENT_NAME"
+echo "   feature branch: $FEATURE_BRANCH_NAME"
 echo ""
+
+# Confirm that the component should be created.
+Y_N=""
+while [[ "$Y_N" != "Y" && "$Y_N" != "y" ]]; do
+  read -rp "Continue (Y/N)? " Y_N
+  echo ""
+
+  if [[ "$Y_N" == "n" || "$Y_N" == "N" ]]; then
+    echo "Component creation canceled."
+    exit 255
+  fi
+done
+
+
+echo "Creating new component $COMPONENT_NAME"
+
+exit -1
+
+# Create a new feature branch for the component from the development branch
+echo "  Updating development branch..."
+git switch development
+git pull origin development
+echo "  Updated."
+echo "  Creating new feature branch $FEATURE_BRANCH_NAME from development..."
+git branch "$FEATURE_BRANCH_NAME"
+echo "  Created."
+echo "  Switching to feature branch $FEATURE_BRANCH_NAME..."
+git switch "$FEATURE_BRANCH_NAME"
+echo "  Switched.
+
+echo "Crated."
+
+# Check that the development branch is checked out
+# CUR_GIT_BRANCH=$(git branch)
+# if [[ ! "$CUR_GIT_BRANCH" == *"* development"* ]]; then
+#   echo -e "${ON_RED}ERROR:${NO_COLOR} You must have the development branch checked out to add an entry point."
+#   echo "Switch to the development branch."
+#   echo "Then run this script again."
+#   exit 255
+# fi
+
+
+
