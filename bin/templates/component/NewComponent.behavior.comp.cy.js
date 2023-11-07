96d9cfa6f1f84cd851daf9734dd1f7dae64c3233
Your Name <braught@dickinson.edu>
2023-11-07 15:13:41 -0500

    fix: updates NewComponent templates with additional structure

--- bin/templates/component/NewComponent.behavior.comp.cy.js
+++ bin/templates/component/NewComponent.behavior.comp.cy.js
@@ -1,18 +1,36 @@
 import %COMPONENT_NAME% from '@comps/%COMPONENT_NAME%/%COMPONENT_NAME%.vue';
 
 describe('Test the  DateSelect component behavior', () => {
+  beforeEach(() => {
+    cy.restoreLocalStorage();
+    cy.restoreSessionStorage();
+  });
+
+  afterEach(() => {
+    cy.saveLocalStorage();
+    cy.saveSessionStorage();
+  });
+  
   it('Add tests for behavior here', () => {
     /*
      * See `components/README.md` for information about component testing.
      * See other components in the `components/` directory for examples.
      */
 
-    cy.mount(%COMPONENT_NAME%);
+    const readySpy = cy.spy().as('readySpy');
 
-    cy.get('[data-cy="new-comp-group"]').should('exist');
-    cy.get('[data-cy="placeholder"]').should(
-      'have.text',
-      'Component content goes here.'
-    );
+    cy.mount(%COMPONENT_NAME%, {
+      props: {
+        onReady: readySpy,
+      },
+    });
+
+    cy.get('@readySpy').should('have.been.calledOnce').then(() => {
+      cy.get('[data-cy="new-comp-group"]').should('exist');
+      cy.get('[data-cy="placeholder"]').should(
+        'have.text',
+        'Component content goes here.'
+      );
+    });
   });
 });
