e2ffcea47825172c8dfb8b7e487caf2bb5be00f9
Your Name <braught@dickinson.edu>
2023-11-11 16:57:31 -0500

    fix: standardizes the use of validity styling

--- library/uiUtil/uiUtil.showInvalidStyling.unit.cy.js
+++ library/uiUtil/uiUtil.showInvalidStyling.unit.cy.js
@@ -1,6 +1,14 @@
 import * as uiUtil from './uiUtil.js';
 
-describe('Test the showInvalidStyling function', () => {
+describe('Test the validStyling function', () => {
+  it('Test the four cases that exist', () => {
+    //                            iValid show
+    expect(uiUtil.validityStyling(false, false)).to.be.null;
+    expect(uiUtil.validityStyling(false, true)).to.be.false;
+    expect(uiUtil.validityStyling(true, false)).to.be.null;
+    expect(uiUtil.validityStyling(true, true)).to.be.true;
+  });
+
   it('Test ', () => {
     // valid value, show invalid styling.
     expect(uiUtil.validationStyling(true, true)).to.be.true;
