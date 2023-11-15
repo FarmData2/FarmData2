7e9ad7a8f9b7edba88db09f52309d76ccec13157
Grant Braught <braught@dickinson.edu>
2023-11-15 11:38:55 -0500

    feat(comps): adds the SubmitResetButtons component (#50)
    **Pull Request Description**
    
    Adds the SubmitResetButtons component to `components`. The
    SubmitResetButtons component provides the Submit and Reset buttons that
    appear at the bottom of all forms.
    
    ---
    
    **Licensing Certification**
    
    FarmData2 is a [Free Cultural
    Work](https://freedomdefined.org/Definition) and all accepted
    contributions are licensed as described in the LICENSE.md file. This
    requires that the contributor holds the rights to do so. By submitting
    this pull request **I certify that I satisfy the terms of the [Developer
    Certificate of Origin](https://developercertificate.org/)** for its
    contents.

---
+++ components/SubmitResetButtons/SubmitResetButtons.events.comp.cy.js
@@ -0,0 +1,93 @@
+import SubmitResetButtons from '@comps/SubmitResetButtons/SubmitResetButtons.vue';
+
+describe('Test the SubmitResetButtons component events', () => {
+  beforeEach(() => {
+    cy.restoreLocalStorage();
+    cy.restoreSessionStorage();
+  });
+
+  afterEach(() => {
+    cy.saveLocalStorage();
+    cy.saveSessionStorage();
+  });
+
+  it('Check submitEnabled prop is watched', () => {
+    const readySpy = cy.spy().as('readySpy');
+
+    cy.mount(SubmitResetButtons, {
+      props: {
+        onReady: readySpy,
+      },
+    }).then(({ wrapper }) => {
+      cy.get('@readySpy')
+        .should('have.been.calledOnce')
+        .then(() => {
+          cy.get('[data-cy="submit-button"]').should('be.disabled');
+
+          wrapper.setProps({ enableSubmit: true });
+
+          cy.get('[data-cy="submit-button"]').should('not.be.disabled');
+        });
+    });
+  });
+
+  it('Check resetEnabled prop is watched', () => {
+    const readySpy = cy.spy().as('readySpy');
+
+    cy.mount(SubmitResetButtons, {
+      props: {
+        onReady: readySpy,
+      },
+    }).then(({ wrapper }) => {
+      cy.get('@readySpy')
+        .should('have.been.calledOnce')
+        .then(() => {
+          cy.get('[data-cy="reset-button"]').should('be.disabled');
+
+          wrapper.setProps({ enableReset: true });
+
+          cy.get('[data-cy="reset-button"]').should('not.be.disabled');
+        });
+    });
+  });
+
+  it('Emits "submit" when submit button is clicked', () => {
+    const readySpy = cy.spy().as('readySpy');
+    const submitSpy = cy.spy().as('submitSpy');
+
+    cy.mount(SubmitResetButtons, {
+      props: {
+        enableSubmit: true,
+        onSubmit: submitSpy,
+        onReady: readySpy,
+      },
+    });
+
+    cy.get('@readySpy')
+      .should('have.been.calledOnce')
+      .then(() => {
+        cy.get('[data-cy="submit-button"]').click();
+        cy.get('@submitSpy').should('have.been.calledOnce');
+      });
+  });
+
+  it('Emits "reset" when reset button is clicked', () => {
+    const readySpy = cy.spy().as('readySpy');
+    const resetSpy = cy.spy().as('resetSpy');
+
+    cy.mount(SubmitResetButtons, {
+      props: {
+        enableReset: true,
+        onReset: resetSpy,
+        onReady: readySpy,
+      },
+    });
+
+    cy.get('@readySpy')
+      .should('have.been.calledOnce')
+      .then(() => {
+        cy.get('[data-cy="reset-button"]').click();
+        cy.get('@resetSpy').should('have.been.calledOnce');
+      });
+  });
+});
