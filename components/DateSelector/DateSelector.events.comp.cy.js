dc1690172a1e97142ee889d99a73f87275ebfd5f
Grant Braught <braught@dickinson.edu>
2023-11-15 11:31:40 -0500

    feat(comps): adds the DateSelector component (#48)
    **Pull Request Description**
    
    Adds the DateSelector component to `components`. The DateSelector
    provides a date input UI element for the user to select a date.
    
    ---
    
    **Licensing Certification**
    
    FarmData2 is a [Free Cultural
    Work](https://freedomdefined.org/Definition) and all accepted
    contributions are licensed as described in the LICENSE.md file. This
    requires that the contributor holds the rights to do so. By submitting
    this pull request **I certify that I satisfy the terms of the [Developer
    Certificate of Origin](https://developercertificate.org/)** for its
    contents.

---
+++ components/DateSelector/DateSelector.events.comp.cy.js
@@ -0,0 +1,175 @@
+import DateSelector from '@comps/DateSelector/DateSelector.vue';
+
+describe('Test the  DateSelector component events', () => {
+  beforeEach(() => {
+    cy.restoreLocalStorage();
+    cy.restoreSessionStorage();
+  });
+
+  afterEach(() => {
+    cy.saveLocalStorage();
+    cy.saveSessionStorage();
+  });
+
+  it('Emits "valid" on creation', () => {
+    const readySpy = cy.spy().as('readySpy');
+    const validSpy = cy.spy().as('validSpy');
+
+    cy.mount(DateSelector, {
+      props: {
+        onReady: readySpy,
+        onValid: validSpy,
+      },
+    });
+
+    cy.get('@readySpy')
+      .should('have.been.calledOnce')
+      .then(() => {
+        cy.get('@validSpy').should('have.been.calledOnce');
+        cy.get('@validSpy').should('have.been.calledWith', true);
+      });
+  });
+
+  it('Verify that `date` prop is watched', () => {
+    const readySpy = cy.spy().as('readySpy');
+    const updateSpy = cy.spy().as('updateSpy');
+
+    cy.mount(DateSelector, {
+      props: {
+        onReady: readySpy,
+        'onUpdate:date': updateSpy,
+      },
+    }).then(({ wrapper }) => {
+      cy.get('@readySpy')
+        .should('have.been.calledOnce')
+        .then(() => {
+          wrapper.setProps({ date: '1999-01-02' });
+          cy.get('@updateSpy').should('have.been.calledOnce');
+          cy.get('[data-cy="date-input"]').should('have.value', '1999-01-02');
+        });
+    });
+  });
+
+  it('Emits "update:date" when date is changed', () => {
+    const readySpy = cy.spy().as('readySpy');
+    const updateSpy = cy.spy().as('updateSpy');
+
+    cy.mount(DateSelector, {
+      props: {
+        onReady: readySpy,
+        'onUpdate:date': updateSpy,
+      },
+    });
+
+    cy.get('@readySpy')
+      .should('have.been.calledOnce')
+      .then(() => {
+        cy.get('[data-cy="date-input"]').type('1999-01-02');
+        cy.get('@updateSpy').should('have.been.calledOnce');
+        cy.get('@updateSpy').should('have.been.calledWith', '1999-01-02');
+      });
+  });
+
+  it('Component reacts to changed showValidityStyling prop', () => {
+    const readySpy = cy.spy().as('readySpy');
+
+    cy.mount(DateSelector, {
+      props: {
+        required: true,
+        date: 'invalid-date',
+        onReady: readySpy,
+      },
+    }).then(({ wrapper }) => {
+      cy.get('@readySpy')
+        .should('have.been.calledOnce')
+        .then(() => {
+          cy.get('[data-cy="date-input"]').should('not.have.class', 'is-valid');
+          cy.get('[data-cy="date-input"]').should(
+            'not.have.class',
+            'is-invalid'
+          );
+
+          wrapper.setProps({ showValidityStyling: true });
+
+          cy.get('[data-cy="date-input"]').should('not.have.class', 'is-valid');
+          cy.get('[data-cy="date-input"]').should('have.class', 'is-invalid');
+        });
+    });
+  });
+
+  it('Emits "valid" when validity of date becomes false', () => {
+    const readySpy = cy.spy().as('readySpy');
+    const validSpy = cy.spy().as('validSpy');
+
+    cy.mount(DateSelector, {
+      props: {
+        required: true,
+        onReady: readySpy,
+        onValid: validSpy,
+      },
+    });
+
+    cy.get('@readySpy')
+      .should('have.been.calledOnce')
+      .then(() => {
+        cy.get('@validSpy').should('have.been.calledOnce');
+        cy.get('@validSpy').should('have.been.calledWith', true);
+
+        cy.get('[data-cy="date-input"]').clear();
+
+        cy.get('@validSpy').should('have.been.calledTwice');
+        cy.get('@validSpy').should('have.been.calledWith', false);
+      });
+  });
+
+  it('Emits "valid" when validity of date becomes true', () => {
+    const readySpy = cy.spy().as('readySpy');
+    const validSpy = cy.spy().as('validSpy');
+
+    cy.mount(DateSelector, {
+      props: {
+        required: true,
+        onReady: readySpy,
+        onValid: validSpy,
+        date: 'invalid-date',
+      },
+    });
+
+    cy.get('@readySpy')
+      .should('have.been.calledOnce')
+      .then(() => {
+        cy.get('@validSpy').should('have.been.calledOnce');
+        cy.get('@validSpy').should('have.been.calledWith', false);
+
+        cy.get('[data-cy="date-input"]').type('1999-01-02');
+
+        cy.get('@validSpy').should('have.been.calledTwice');
+        cy.get('@validSpy').should('have.been.calledWith', false);
+      });
+  });
+
+  it('Only emits "valid" when validity of date changes', () => {
+    const readySpy = cy.spy().as('readySpy');
+    const validSpy = cy.spy().as('validSpy');
+
+    cy.mount(DateSelector, {
+      props: {
+        required: true,
+        onReady: readySpy,
+        onValid: validSpy,
+      },
+    });
+
+    cy.get('@readySpy')
+      .should('have.been.calledOnce')
+      .then(() => {
+        cy.get('@validSpy').should('have.been.calledOnce');
+
+        cy.get('[data-cy="date-input"]').type('1999-01-02');
+        cy.get('[data-cy="date-input"]').type('1999-01-03');
+        cy.get('[data-cy="date-input"]').type('1999-01-04');
+
+        cy.get('@validSpy').should('have.been.calledOnce');
+      });
+  });
+});
---
+++ components/DateSelector/DateSelector.vue
@@ -0,0 +1,146 @@
+<template>
+  <BFormGroup
+    id="date-group"
+    data-cy="date-group"
+    label-for="date"
+    label-cols="auto"
+    label-align="end"
+  >
+    <template v-slot:label>
+      <span data-cy="date-label">Date:</span>
+      <sup
+        data-cy="required-star"
+        v-if="required"
+        class="text-danger"
+        >*</sup
+      >
+    </template>
+
+    <BFormInput
+      id="date-input"
+      data-cy="date-input"
+      type="date"
+      v-model="chosenDate"
+      v-bind:state="validityStyling"
+      v-bind:required="required"
+    />
+    <BFormInvalidFeedback
+      id="date-invalid-feedback"
+      data-cy="date-invalid-feedback"
+      v-bind:state="validityStyling"
+    >
+      A valid date is required.
+    </BFormInvalidFeedback>
+  </BFormGroup>
+</template>
+
+<script>
+import dayjs from 'dayjs';
+
+/**
+ * The DateSelector component provides a UI element for selecting a date.
+ *
+ * ## Usage Example
+ *
+ * ```html
+ * <DateSelector
+ *   required
+ *   v-model:date="form.seedingDate"
+ *   v-bind:showInvalidStyling="validity.show"
+ *   v-on:valid="validity.seedingDate = $event"
+ *   v-on:ready="createdCount++"
+ * />
+ * ```
+ *
+ * ## `data-cy` Attributes
+ *
+ * Attribute Name          | Description
+ * ------------------------| -----------
+ * date-group              | The `BFormGroup` component containing this component.
+ * date-label              | The `span` component containing the "Date:" label.
+ * required-star           | The `*` that appears in the label if the input is required.
+ * date-input              | The `BFormInput` component used to select a date.
+ * date-invalid-feedback   | The `BFormInvalidFeedback` component that displays help when the date is invalid.
+ */
+export default {
+  name: 'DateSelector',
+  emits: ['ready', 'update:date', 'valid'],
+  props: {
+    /**
+     * The selected date.
+     * This prop is watched and changes are relayed to the component's internal state..
+     */
+    date: {
+      type: String,
+      default: dayjs().format('YYYY-MM-DD'), // default to today.
+    },
+    /**
+     * Whether a crop selection is required or not.
+     */
+    required: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * Whether validity styling should appear on input elements.
+     */
+    showValidityStyling: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      chosenDate: this.date,
+    };
+  },
+  computed: {
+    isValid() {
+      return dayjs(this.chosenDate).isValid();
+    },
+    // Controls component styling (i.e. when green check or red X and invalid feedback) should be displayed.
+    validityStyling() {
+      const R = this.required;
+      const V = this.isValid;
+      const S = this.showValidityStyling;
+
+      if (V && S) {
+        return true;
+      } else if (R && !V && S) {
+        return false;
+      } else {
+        return null;
+      }
+    },
+  },
+  methods: {},
+  watch: {
+    chosenDate() {
+      /**
+       * The selected date has changed.
+       * @property {String} date the newly chosen date (YYYY-MM-DD).
+       */
+      this.$emit('update:date', this.chosenDate);
+    },
+    date() {
+      this.chosenDate = this.date;
+    },
+    isValid() {
+      /**
+       * The validity of the date has changed.
+       * @property {Boolean} valid whether the date is valid or not.
+       */
+      this.$emit('valid', this.isValid);
+    },
+  },
+  created() {
+    // Emit the initial valid state of the component's value.
+    this.$emit('valid', this.isValid);
+
+    /**
+     * The component is ready for use.
+     */
+    this.$emit('ready');
+  },
+};
+</script>
