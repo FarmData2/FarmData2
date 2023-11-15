1be9f683419004d72dd44365b9625fd83ecf7ec9
Grant Braught <braught@dickinson.edu>
2023-11-15 09:23:18 -0500

    build: updates entrypoints to include entry_point_name.html files

--- modules/farm_fd2/vite.config.js
+++ modules/farm_fd2/vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url';
-import path from 'node:path';
 import glob from 'glob';
 import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
@@ -39,20 +38,17 @@ let viteConfig = {
       // the live farmos server shows the most recent content.
       name: 'afterBuild',
       closeBundle: async () => {
-        await exec(
-          'docker exec fd2_farmos drush cr',
-          (error, stderr, stdout) => {
-            if (error) {
-              console.error(`error:  ${error.message}`);
-              return;
-            }
-            if (stderr) {
-              console.error(`stderr: ${stderr}`);
-              return;
-            }
-            console.log(`Rebuilding drupal cache...\n  ${stdout}`);
+        exec('docker exec fd2_farmos drush cr', (error, stderr, stdout) => {
+          if (error) {
+            console.error(`error:  ${error.message}`);
+            return;
           }
-        );
+          if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return;
+          }
+          console.log(`Rebuilding drupal cache...\n  ${stdout}`);
+        });
       },
     },
   ],
@@ -62,17 +58,23 @@ let viteConfig = {
     exclude: ['**/*.cy.js', '**/*.cy.comp.js', '**/*.cy.unit.js'],
     rollupOptions: {
       input: Object.fromEntries(
-        glob
-          .sync('modules/farm_fd2/src/entrypoints/*')
-          .map((dir) => [path.basename(dir), dir + '/index.html'])
+        glob.sync('modules/farm_fd2/src/entrypoints/*/*.html').map((dir) => {
+          let key = dir.split('/').at(-2) + '/' + dir.split('/').at(-1);
+          return [key, dir];
+        })
       ),
       output: {
         // Ensures that the entry point and css names are not hashed.
         entryFileNames: '[name]/[name].js',
         assetFileNames: (assetInfo) => {
+          let name = assetInfo.name.split('/').at(0);
           let ext = assetInfo.name.split('.').at(1);
           if (ext === 'css') {
-            return 'shared/[name].css';
+            if (name.startsWith('_plugin-vue_')) {
+              return 'shared/index.css';
+            } else {
+              return '[name]/[name].css';
+            }
           } else {
             return 'shared/[name].[ext]';
           }
@@ -85,7 +87,8 @@ let viteConfig = {
     alias: {
       '@': fileURLToPath(new URL('./src/', import.meta.url)),
       '@comps': fileURLToPath(new URL('../../components/', import.meta.url)),
-      '@libs': fileURLToPath(new URL('../../libraries/', import.meta.url)),
+      '@libs': fileURLToPath(new URL('../../library/', import.meta.url)),
+      '@css': fileURLToPath(new URL('../css/', import.meta.url)),
     },
   },
 };
