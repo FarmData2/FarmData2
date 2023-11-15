05a1bfd79921db6b2d42f7226b52a5ad488afa2b
Grant Braught <braught@dickinson.edu>
2023-11-15 11:16:50 -0500

    feat(comps): adds the CropSelector component

---
+++ components/CropSelector/CropSelector.behavior.comp.cy.js
@@ -0,0 +1,36 @@
+import CropSelector from '@comps/CropSelector/CropSelector.vue';
+
+describe('Test the CropSelector behaviors', () => {
+  beforeEach(() => {
+    cy.restoreLocalStorage();
+    cy.restoreSessionStorage();
+  });
+
+  afterEach(() => {
+    cy.saveLocalStorage();
+    cy.saveSessionStorage();
+  });
+
+  it('Clicking add crop button goes to the add crop form', () => {
+    const readySpy = cy.spy().as('readySpy');
+
+    cy.intercept('GET', '**/taxonomy/manage/plant_type/add', {
+      statusCode: 200,
+      body: 'Add Crop Form',
+    }).as('urlIntercept');
+
+    cy.mount(CropSelector, {
+      props: {
+        onReady: readySpy,
+      },
+    }).then(() => {
+      cy.get('@readySpy')
+        .should('have.been.calledOnce')
+        .then(() => {
+          cy.get('[data-cy="selector-add-button"]').should('exist');
+          cy.get('[data-cy="selector-add-button"]').click();
+          cy.wait('@urlIntercept').its('response.statusCode').should('eq', 200);
+        });
+    });
+  });
+});
---
+++ components/CropSelector/CropSelector.vue
@@ -0,0 +1,140 @@
+<template>
+  <SelectorBase
+    id="crop-selector"
+    data-cy="crop-selector"
+    label="Crop"
+    invalidFeedbackText="A crop is required"
+    v-bind:addOptionUrl="addCropUrl"
+    v-bind:options="cropList"
+    v-bind:required="required"
+    v-bind:selected="selected"
+    v-bind:showValidityStyling="showValidityStyling"
+    v-on:update:selected="handleUpdateSelected($event)"
+    v-on:valid="handleValid($event)"
+  />
+</template>
+
+<script>
+import SelectorBase from '@comps/SelectorBase/SelectorBase.vue';
+import * as farmosUtil from '@libs/farmosUtil/farmosUtil.js';
+
+/**
+ * The CropSelector component is a UI element that allows the user to select a crop.
+ *
+ * It fetches the list for crops (farmOS `taxonomy_term--plant_type` records) from the farmOS server.
+ *
+ * ## Usage Example
+ *
+ * ```html
+ * <CropSelector
+ *   required
+ *   v-model:selected="form.crop"
+ *   v-bind:showInvalidStyling="validity.show"
+ *   v-on:valid="validity.crop = $event"
+ *   v-on:ready="createdCount++"
+ *   v-on:error="
+ *     (msg) =>
+ *       uiUtil.showToast('Network Error', msg, 'top-center', 'danger', 5)
+ *   "
+ * />
+ * ```
+ *
+ * ## `data-cy` Attributes
+ *
+ * Attribute Name        | Description
+ * ----------------------| -----------
+ * crop-selector         | The `SelectorBase` component containing the dropdown.
+ */
+export default {
+  name: 'CropSelector',
+  components: { SelectorBase },
+  emits: ['error', 'ready', 'update:selected', 'valid'],
+  props: {
+    /**
+     * Whether a crop selection is required or not.
+     */
+    required: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * The name of the selected crop.
+     * This prop is watched and changes are relayed to the component's internal state.
+     */
+    selected: {
+      type: String,
+      default: null,
+    },
+    /**
+     * Whether validity styling should appear on input elements.
+     */
+    showValidityStyling: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      cropList: [],
+    };
+  },
+  computed: {
+    addCropUrl() {
+      return '/admin/structure/taxonomy/manage/plant_type/add';
+    },
+  },
+  methods: {
+    handleUpdateSelected(event) {
+      /**
+       * The selected crop has changed.
+       * @property {string} event the name of the new selected crop.
+       */
+      this.$emit('update:selected', event);
+    },
+    handleValid(event) {
+      /**
+       * The validity of the selected crop has changed.
+       * @property {boolean} event whether the selected crop is valid or not.
+       */
+      this.$emit('valid', event);
+    },
+  },
+  watch: {},
+  created() {
+    farmosUtil
+      .getFarmOSInstance()
+      .then((farm) => {
+        farmosUtil
+          .getCropNameToTermMap(farm)
+          .then((cropMap) => {
+            this.cropList = Array.from(cropMap.keys());
+
+            /**
+             * The select has been populated with the list of crops and the component is ready to be used.
+             */
+            this.$emit('ready');
+          })
+          .catch((error) => {
+            console.error('CropSelector: Error fetching crops.');
+            console.error(error);
+            /**
+             * An error occurred when communicating with the farmOS server.
+             * @property {string} msg an error message.
+             */
+            this.$emit('error', 'Unable to fetch crops.');
+          });
+      })
+      .catch((error) => {
+        console.error('CropSelector: Error connecting to farm.');
+        console.error(error);
+        this.$emit('error', 'Unable to connect to farmOS server.');
+      });
+  },
+};
+</script>
+
+<style scoped>
+#crop-group {
+  min-width: 215px;
+}
+</style>
