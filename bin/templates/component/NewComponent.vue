7fa4a06268a9b93ceac00f5b8b999527c59b9b00
Grant Braught <braught@dickinson.edu>
2023-11-16 10:43:37 -0500

    fix(comps): updates the addComponent.bash script and templates (#53)
    **Pull Request Description**
    
    Makes updates to the script and templates used to add a new component.
    - Adds a `styling` test template
    - Documents `styling` tests in `components/README.md` draft
    - Updates `addComponent.bash` to work with `styling` tests template
    - Updates contents of other `.comp.cy.js` template files also.
    
    ---
    
    **Licensing Certification**
    
    FarmData2 is a [Free Cultural
    Work](https://freedomdefined.org/Definition) and all accepted
    contributions are licensed as described in the LICENSE.md file. This
    requires that the contributor holds the rights to do so. By submitting
    this pull request **I certify that I satisfy the terms of the [Developer
    Certificate of Origin](https://developercertificate.org/)** for its
    contents.

--- bin/templates/component/NewComponent.vue
+++ bin/templates/component/NewComponent.vue
@@ -39,7 +39,7 @@ export default {
     /**
      * Whether validity styling should appear on input elements.
      */
-    showInvalidStyling: {
+    showValidityStyling: {
       type: Boolean,
       default: false,
     },
@@ -51,15 +51,17 @@ export default {
     isValid() {
       /*
        * Edit this computed property to return true if the component's value is valid,
-       * or false if it is invalid.
+       * or false if it is invalid.  This should account for whether the value is 
+       * required or not if necessary.
        */
       return false;
     },
     // Controls component styling (i.e. when green check or red X and invalid feedback) should be displayed.
-    validationStyling() {
+    validityStyling() {
       /*
        * Edit this computed property to indicted the type of styling that should be applied 
-       * to the component based upon `required`, `isValid`, `showInvalidStyling`.
+       * to the component based upon `required`, `isValid`, `showInvalidStyling`, and any
+       * other criteria that is necessary.
        * 
        * Bind this computed property to the `state` prop of the components to be styled.
        */
@@ -71,7 +73,7 @@ export default {
     isValid() {
       /**
        * The validity of the component has changed.  Also emitted when the component is created.
-       * @property {*} valid `true` if the component's value is valid; `false` if it is invalid.
+       * @property {Boolean} valid `true` if the component's value is valid; `false` if it is invalid.
        */
       this.$emit('valid', this.isValid);
     },
