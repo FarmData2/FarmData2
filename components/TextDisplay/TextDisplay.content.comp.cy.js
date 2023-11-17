54b25545b873405b6f880403f1a6c1a147e10b21
Grant Braught <braught@dickinson.edu>
2023-11-17 11:26:16 -0500

    feat(comps): adds TextDisplay component (#56)
    **Pull Request Description**
    
    Adds the TextDisplay component to `components`. The TextDisplay
    component is used to display non-editable labeled text in the UI. For
    example, a value computed from other values entered into a form (e.g.
    Total Seeds in the tray seeding form).
    
    ---
    
    **Licensing Certification**
    
    FarmData2 is a [Free Cultural
    Work](https://freedomdefined.org/Definition) and all accepted
    contributions are licensed as described in the LICENSE.md file. This
    requires that the contributor holds the rights to do so. By submitting
    this pull request **I certify that I satisfy the terms of the [Developer
    Certificate of Origin](https://developercertificate.org/)** for its
    contents.

---
+++ components/TextDisplay/TextDisplay.content.comp.cy.js
@@ -0,0 +1,70 @@
+import TextDisplay from '@comps/TextDisplay/TextDisplay.vue';
+
+describe('Test the default TextDisplay content', () => {
+  beforeEach(() => {
+    cy.restoreLocalStorage();
+    cy.restoreSessionStorage();
+  });
+
+  afterEach(() => {
+    cy.saveLocalStorage();
+    cy.saveSessionStorage();
+  });
+
+  it('Check all of the default data-cy elements', () => {
+    const readySpy = cy.spy().as('readySpy');
+
+    cy.mount(TextDisplay, {
+      props: {
+        label: 'Test Label',
+        text: 'Test Text',
+        onReady: readySpy,
+      },
+    });
+
+    cy.get('@readySpy')
+      .should('have.been.calledOnce')
+      .then(() => {
+        cy.get('[data-cy="text-label"]').should('have.text', 'Test Label:');
+        cy.get('[data-cy="text-text"]').should('have.value', 'Test Text');
+      });
+  });
+
+  it('Check null text renders as empty string', () => {
+    const readySpy = cy.spy().as('readySpy');
+
+    cy.mount(TextDisplay, {
+      props: {
+        label: 'Test Label',
+        text: null,
+        onReady: readySpy,
+      },
+    });
+
+    cy.get('@readySpy')
+      .should('have.been.calledOnce')
+      .then(() => {
+        cy.get('[data-cy="text-label"]').should('have.text', 'Test Label:');
+        cy.get('[data-cy="text-text"]').should('have.value', '');
+      });
+  });
+
+  it('Check NaN text renders as empty string', () => {
+    const readySpy = cy.spy().as('readySpy');
+
+    cy.mount(TextDisplay, {
+      props: {
+        label: 'Test Label',
+        text: 'NaN',
+        onReady: readySpy,
+      },
+    });
+
+    cy.get('@readySpy')
+      .should('have.been.calledOnce')
+      .then(() => {
+        cy.get('[data-cy="text-label"]').should('have.text', 'Test Label:');
+        cy.get('[data-cy="text-text"]').should('have.value', '');
+      });
+  });
+});
