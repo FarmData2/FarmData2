7659fcea76025f8b8e3b8cca6e4b0cfdbebbdf7a
Grant Braught <braught@dickinson.edu>
2023-11-17 11:26:16 -0500

    feat(comps): adds TextDisplay component (#56)
    **Pull Request Description**
    
    Adds the TextDisplay component to `components`. The TextDisplay
    component is used to display non-editable labeled text in the UI. For
    example, a value computed from other values entered into a form (e.g.
    Total Seeds in the tray seeding form).
    
    ---
    
    **Licensing Certification**
    
    FarmData2 is a [Free Cultural
    Work](https://freedomdefined.org/Definition) and all accepted
    contributions are licensed as described in the LICENSE.md file. This
    requires that the contributor holds the rights to do so. By submitting
    this pull request **I certify that I satisfy the terms of the [Developer
    Certificate of Origin](https://developercertificate.org/)** for its
    contents.

---
+++ components/TextDisplay/TextDisplay.vue
@@ -0,0 +1,88 @@
+<template>
+  <BFormGroup
+    id="text-group"
+    data-cy="text-group"
+    v-bind:label="label"
+    label-for="text-text"
+    label-cols="auto"
+  >
+    <template v-slot:label>
+      <span data-cy="text-label">{{ label }}:</span>
+    </template>
+    <BFormInput
+      id="text-text"
+      data-cy="text-text"
+      disabled
+      plaintext
+      v-model="displayText"
+    />
+  </BFormGroup>
+</template>
+
+<script>
+/**
+ * A TextDisplay component provides a UI element for displaying a labeled text string.
+ *
+ * The text is displayed in a disabled `<BFormInput>` element.
+ *
+ * If the `text` prop is `null` or is the string `'NaN'`, the text will be displayed as an empty string.
+ *
+ * ## Usage Example
+ *
+ * ```html
+ * Add example of how to add this component to a template.
+ * See the other components in the `components` directory for examples.
+ * ```
+ *
+ * ## `data-cy` Attributes
+ *
+ * Attribute Name        | Description
+ * ----------------------| -----------
+ * `text-group`          | the `BFormGroup` component containing the text display.
+ * `text-label`          | the `span` component containing the label for the displayed text.
+ * `text-text`           | the disabled `BFormInput` component used to display the text.
+ */
+export default {
+  name: 'TextDisplay',
+  components: {},
+  emits: ['ready'],
+  props: {
+    /**
+     * The label for the text that is displayed.
+     */
+    label: {
+      type: String,
+      required: true,
+    },
+    /**
+     * The text that is to be displayed.
+     */
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      displayText: this.processedText(),
+    };
+  },
+  computed: {},
+  methods: {
+    processedText() {
+      if (this.text == null || this.text == 'NaN') {
+        return '';
+      } else {
+        return this.text;
+      }
+    },
+  },
+  watch: {},
+  created() {
+    /**
+     * The component is ready for use.
+     */
+    this.$emit('ready');
+  },
+};
+</script>
