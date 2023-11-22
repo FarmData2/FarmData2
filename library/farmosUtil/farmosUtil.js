f117afdb97f2fa9bc832032a1c243092130d31c3
Grant Braught <braught@dickinson.edu>
2023-11-22 12:19:56 -0500

    refactor(libs): caches fetched data (#69)
    **Pull Request Description**
    
    All of the functions that make API requests have been updated to cache
    their results in both a global variable (as a JS object) and in the
    `sessionStorage` as stringified JSON. This ensures that when multiple
    components or library functions make requests for the same data, an
    actual API request only occurs once.
    
    Closes #68 
    
    ---
    
    **Licensing Certification**
    
    FarmData2 is a [Free Cultural
    Work](https://freedomdefined.org/Definition) and all accepted
    contributions are licensed as described in the LICENSE.md file. This
    requires that the contributor holds the rights to do so. By submitting
    this pull request **I certify that I satisfy the terms of the [Developer
    Certificate of Origin](https://developercertificate.org/)** for its
    contents.

--- library/farmosUtil/farmosUtil.js
+++ library/farmosUtil/farmosUtil.js
@@ -34,9 +34,20 @@ export function clearFarmGlobal() {
   global_farm = null;
 }
 
+/**
+ * @private
+ *
+ * Get the `global_farm` object.  This is useful for testing to ensure
+ * that the global is set at appropriate times.
+ *
+ * @returns the `global_farm` object
+ */
 export function getFarmGlobal() {
   return global_farm;
 }
+
+var libLocalStorage = null;
+var libSessionStorage = null;
 
 /**
  * Get an instance of the `farmos.js` `farmOS` object that can be used
@@ -65,21 +76,23 @@ export async function getFarmOSInstance(
   ls = null
 ) {
   /*
-   * Handle local and session storage here so that this function
-   * can be used in both Node and in the browser (live and w/ Cypress).
+   * Handle local and session storage here so that the functions
+   * in this library can be used in both Node and in the browser
+   * (live and w/ Cypress).
    */
-  let myLocalStorage = ls;
-  let mySessionStorage = ls;
+  libLocalStorage = ls;
+  libSessionStorage = ls;
   if (!ls) {
-    myLocalStorage = localStorage;
-    mySessionStorage = sessionStorage;
+    libLocalStorage = localStorage;
+    libSessionStorage = sessionStorage;
   }
 
   const config = {
     host: hostURL,
     clientId: client,
-    getToken: () => JSON.parse(myLocalStorage.getItem('token')),
-    setToken: (token) => myLocalStorage.setItem('token', JSON.stringify(token)),
+    getToken: () => JSON.parse(libLocalStorage.getItem('token')),
+    setToken: (token) =>
+      libLocalStorage.setItem('token', JSON.stringify(token)),
   };
   const options = { remote: config };
 
@@ -110,102 +123,18 @@ export async function getFarmOSInstance(
   // Only set the schema if this is a new farm object.
   if (newfarm) {
     //Try the session storage first...
-    let schema = JSON.parse(mySessionStorage.getItem('schema'));
+    let schema = JSON.parse(libSessionStorage.getItem('schema'));
     if (schema == null) {
       // Not in session storage, so fetch schema from the farmOS host.
       await global_farm.schema.fetch();
       schema = global_farm.schema.get();
       // Cache in the session storage for next time.
-      mySessionStorage.setItem('schema', JSON.stringify(schema));
+      libSessionStorage.setItem('schema', JSON.stringify(schema));
     }
     await global_farm.schema.set(schema);
   }
 
   return global_farm;
-}
-
-/**
- * Print out the JSON structure of the specified farmOS record type.
- * (e.g. asset--land, log--harvest, etc...  This is useful as a development
- * and debugging tool.
- *
- * @param {object} farm a `farmOS` object returned from `getFarmOSInstance`.
- * @param {string} recordType the type of farmOS record to display.
- */
-export function printObject(farm, recordType) {
-  const obj = farm.log.create({ type: recordType });
-  console.dir(obj);
-}
-
-/**
- * Get an array containing all of the active users from the farmOS host.  The users
- * will appear in the array in order by the value of the `attributes.display_name`
- * property.
- *
- * NOTE: The `Anonymous` user does not appear in the returned array.
- *
- * NOTE: This function makes an API call to the farmOS host.  Thus,
- * if the array is to be used multiple times it should be cached
- * by the calling code.
- *
- * @param {object} farm a `farmOS` object returned from `getFarmOSInstance`.
- * @returns an array of farmOS `user--user` objects.
- */
-export async function getUsers(farm) {
-  const users = await farm.user.fetch({
-    filter: {
-      type: 'user--user',
-      status: true,
-    },
-    limit: Infinity,
-  });
-
-  if (users.rejected.length != 0) {
-    throw new Error('Unable to fetch users.', users.rejected);
-  }
-
-  users.data.sort((o1, o2) =>
-    o1.attributes.display_name.localeCompare(o2.attributes.display_name)
-  );
-
-  return users.data;
-}
-
-/**
- * Get a map from the user 'display_name` to the corresponding
- * farmOS user object.
- *
- * NOTE: The returned `Map` is built on the value returned by
- * [getUsers]{@link #module_farmosUtil.getUsers}.
- *
- * @param {object} farm a `farmOS` object returned from `getFarmOSInstance`.
- * @returns an `Map` from the user `display_name` to the `user--user` object.
- */
-export async function getUsernameToUserMap(farm) {
-  const users = await getUsers(farm);
-
-  const map = new Map(
-    users.map((user) => [user.attributes.display_name, user])
-  );
-
-  return map;
-}
-
-/**
- * Get a map from the user `id` to the farmOS user object.
- *
- * NOTE: The returned `Map` is built on the value returned by
- * [getUsers]{@link #module_farmosUtil.getUsers}.
- *
- * @param {object} farm a `farmOS` object returned from `getFarmOSInstance`.
- * @returns an `Map` from the user `display_name` to the `user--user` object.
- */
-export async function getUserIdToUserMap(farm) {
-  const users = await getUsers(farm);
-
-  const map = new Map(users.map((user) => [user.id, user]));
-
-  return map;
 }
 
 /**
@@ -259,14 +188,159 @@ export function addParentRelationship(obj, parentId, parentType) {
 }
 
 /**
+ * Print out the JSON structure of the specified farmOS record type.
+ * (e.g. asset--land, log--harvest, etc...  This is useful as a development
+ * and debugging tool.
+ *
+ * @param {object} farm a `farmOS` object returned from `getFarmOSInstance`.
+ * @param {string} recordType the type of farmOS record to display.
+ */
+export function printObject(farm, recordType) {
+  const obj = farm.log.create({ type: recordType });
+  console.dir(obj);
+}
+
+// Used to cache result of the getUsers function.
+var global_users = null;
+
+/**
+ * Clear the cached results from prior calls to the `getUsers` function.
+ * This is useful when an action may change the users that exist in the
+ * system
+ */
+export function clearCachedUsers() {
+  global_users = null;
+  libSessionStorage.removeItem('users');
+}
+
+/**
+ * @private
+ *
+ * Get the `global_users` object.  This is useful for testing to ensure
+ * that the global is set by the appropriate functions.
+ *
+ * @returns the `global_users` object
+ */
+export function getGlobalUsers() {
+  return global_users;
+}
+
+/**
+ * Get an array containing all of the active users from the farmOS host.  The users
+ * will appear in the array in order by the value of the `attributes.display_name`
+ * property.
+ *
+ * NOTE: The `Anonymous` user does not appear in the returned array.
+ *
+ * NOTE: The result of this function is cached.
+ * Use the [`clearCachedUsers`]{@link #module_farmosUtil.clearCachedUsers}
+ * function to clear the cache.
+ *
+ * @param {object} farm a `farmOS` object returned from `getFarmOSInstance`.
+ * @returns an array of farmOS `user--user` objects.
+ */
+export async function getUsers(farm) {
+  if (global_users) {
+    return global_users;
+  }
+
+  if (libSessionStorage.getItem('users') != null) {
+    global_users = JSON.parse(libSessionStorage.getItem('users'));
+    return global_users;
+  }
+
+  const users = await farm.user.fetch({
+    filter: {
+      type: 'user--user',
+      status: true,
+    },
+    limit: Infinity,
+  });
+
+  if (users.rejected.length != 0) {
+    throw new Error('Unable to fetch users.', users.rejected);
+  }
+
+  users.data.sort((o1, o2) =>
+    o1.attributes.display_name.localeCompare(o2.attributes.display_name)
+  );
+
+  libSessionStorage.setItem('users', JSON.stringify(users.data));
+  global_users = users.data;
+  return global_users;
+}
+
+/**
+ * Get a map from the user 'display_name` to the corresponding
+ * farmOS user object.
+ *
+ * NOTE: The returned `Map` is built on the value returned by
+ * [getUsers]{@link #module_farmosUtil.getUsers}.
+ *
+ * @param {object} farm a `farmOS` object returned from `getFarmOSInstance`.
+ * @returns an `Map` from the user `display_name` to the `user--user` object.
+ */
+export async function getUsernameToUserMap(farm) {
+  const users = await getUsers(farm);
+
+  const map = new Map(
+    users.map((user) => [user.attributes.display_name, user])
+  );
+
+  return map;
+}
+
+/**
+ * Get a map from the user `id` to the farmOS user object.
+ *
+ * NOTE: The returned `Map` is built on the value returned by
+ * [getUsers]{@link #module_farmosUtil.getUsers}.
+ *
+ * @param {object} farm a `farmOS` object returned from `getFarmOSInstance`.
+ * @returns an `Map` from the user `display_name` to the `user--user` object.
+ */
+export async function getUserIdToUserMap(farm) {
+  const users = await getUsers(farm);
+
+  const map = new Map(users.map((user) => [user.id, user]));
+
+  return map;
+}
+
+// Used to cache result of the getFieldsAndBeds function.
+var global_fields_and_beds = null;
+
+/**
+ * Clear the cached results from prior calls to the `getFieldsAndBeds` function.
+ * This is useful when an action may change the users that exist in the
+ * system
+ */
+export function clearCachedFieldsAndBeds() {
+  global_fields_and_beds = null;
+  libSessionStorage.removeItem('fields_and_beds');
+}
+
+/**
+ * @private
+ *
+ * Get the `global_fields_and_beds` object.  This is useful for testing to ensure
+ * that the global is set by the appropriate functions.
+ *
+ * @returns the `global_fields_and_beds` object
+ */
+export function getGlobalFieldsAndBeds() {
+  return global_fields_and_beds;
+}
+
+/**
  * Get the asset objects for all of the active places that represent fields or beds.
  * These are the assets of type `asset--land` that have `land_type` of either
  * `field` or `bed`.  The fields and beds will appear in alphabetical order
  * by the value of the `attributes.name` property.
  *
- * NOTE: This function makes an API call to the farmOS host.  Thus,
- * if the array is to be used multiple times it should be cached
- * by the calling code.
+ * NOTE: The result of this function is cached.
+ * Use the [`clearCachedFieldsAndBeds`]{@link #module_farmosUtil.clearCachedFieldsAndBeds}
+ * function to clear the cache.
  *
  * @param {object} farm a `farmOS` object returned from `getFarmOSInstance`.
  * @returns a array of all of land assets representing fields or beds.
@@ -274,6 +348,17 @@ export function addParentRelationship(obj, parentId, parentType) {
 export async function getFieldsAndBeds(farm) {
   // Done as two requests for now because of a bug in the farmOS.js library.
   // https://github.com/farmOS/farmOS.js/issues/86
+
+  if (global_fields_and_beds) {
+    return global_fields_and_beds;
+  }
+
+  if (libSessionStorage.getItem('fields_and_beds') != null) {
+    global_fields_and_beds = JSON.parse(
+      libSessionStorage.getItem('fields_and_beds')
+    );
+    return global_fields_and_beds;
+  }
 
   const beds = await farm.asset.fetch({
     filter: {
@@ -301,7 +386,9 @@ export async function getFieldsAndBeds(farm) {
   const land = fields.data.concat(beds.data);
   land.sort((o1, o2) => o1.attributes.name.localeCompare(o2.attributes.name));
 
-  return land;
+  libSessionStorage.setItem('fields_and_beds', JSON.stringify(land));
+  global_fields_and_beds = land;
+  return global_fields_and_beds;
 }
 
 /**
@@ -340,6 +427,31 @@ export async function getFieldOrBedIdToAssetMap(farm) {
   return map;
 }
 
+// Used to cache result of the getGreenhouses function.
+var global_greenhouses = null;
+
+/**
+ * Clear the cached results from prior calls to the `getGreenhouses` function.
+ * This is useful when an action may change the users that exist in the
+ * system
+ */
+export function clearCachedGreenhouses() {
+  global_greenhouses = null;
+  libSessionStorage.removeItem('greenhouses');
+}
+
+/**
+ * @private
+ *
+ * Get the `global_greenhouses` object.  This is useful for testing to ensure
+ * that the global is set by the appropriate functions.
+ *
+ * @returns the `global_greenhouses` object
+ */
+export function getGlobalGreenhouses() {
+  return global_greenhouses;
+}
+
 /**
  * Get the asset objects for all of the active structures that represent greenhouses.
  * These are the assets of type `asset--structure` that have `structure_type` of
@@ -354,6 +466,15 @@ export async function getFieldOrBedIdToAssetMap(farm) {
  * @returns an array of all of land assets representing greenhouses.
  */
 export async function getGreenhouses(farm) {
+  if (global_greenhouses) {
+    return global_greenhouses;
+  }
+
+  if (libSessionStorage.getItem('greenhouses') != null) {
+    global_greenhouses = JSON.parse(libSessionStorage.getItem('greenhouses'));
+    return global_greenhouses;
+  }
+
   const greenhouses = await farm.asset.fetch({
     filter: {
       type: 'asset--structure',
@@ -371,7 +492,9 @@ export async function getGreenhouses(farm) {
     o1.attributes.name.localeCompare(o2.attributes.name)
   );
 
-  return greenhouses.data;
+  libSessionStorage.setItem('greenhouses', JSON.stringify(greenhouses.data));
+  global_greenhouses = greenhouses.data;
+  return global_greenhouses;
 }
 
 /**
@@ -408,6 +531,31 @@ export async function getGreenhouseIdToAssetMap(farm) {
   return map;
 }
 
+// Used to cache result of the getCrops function.
+var global_crops = null;
+
+/**
+ * Clear the cached results from prior calls to the `getCrops` function.
+ * This is useful when an action may change the users that exist in the
+ * system
+ */
+export function clearCachedCrops() {
+  global_crops = null;
+  libSessionStorage.removeItem('crops');
+}
+
+/**
+ * @private
+ *
+ * Get the `global_crops` object.  This is useful for testing to ensure
+ * that the global is set by the appropriate functions.
+ *
+ * @returns the `global_crops` object
+ */
+export function getGlobalCrops() {
+  return global_crops;
+}
+
 /**
  * Get taxonomy term objects for all of the crops.
  * These are the taxonomy terms of type `taxonomy_term--plant_type`.
@@ -423,6 +571,15 @@ export async function getGreenhouseIdToAssetMap(farm) {
  * @returns an array of all of taxonomy terms representing crops.
  */
 export async function getCrops(farm) {
+  if (global_crops) {
+    return global_crops;
+  }
+
+  if (libSessionStorage.getItem('crops') != null) {
+    global_crops = JSON.parse(libSessionStorage.getItem('crops'));
+    return global_crops;
+  }
+
   const crops = await farm.term.fetch({
     filter: {
       type: 'taxonomy_term--plant_type',
@@ -438,7 +595,9 @@ export async function getCrops(farm) {
     o1.attributes.name.localeCompare(o2.attributes.name)
   );
 
-  return crops.data;
+  libSessionStorage.setItem('crops', JSON.stringify(crops.data));
+  global_crops = crops.data;
+  return global_crops;
 }
 
 /**
@@ -475,6 +634,31 @@ export async function getCropIdToTermMap(farm) {
   return map;
 }
 
+// Used to cache result of the getTraySizes function.
+var global_tray_sizes = null;
+
+/**
+ * Clear the cached results from prior calls to the `getTraySizes` function.
+ * This is useful when an action may change the users that exist in the
+ * system
+ */
+export function clearCachedTraySizes() {
+  global_tray_sizes = null;
+  libSessionStorage.removeItem('tray_sizes');
+}
+
+/**
+ * @private
+ *
+ * Get the `global_tray_sizes` object.  This is useful for testing to ensure
+ * that the global is set by the appropriate functions.
+ *
+ * @returns the `global_tray_sizes` object
+ */
+export function getGlobalTraySizes() {
+  return global_tray_sizes;
+}
+
 /**
  * Get taxonomy term objects for all of the tray sizes.
  * These are the taxonomy terms of type `taxonomy_term--tray_size`.
@@ -490,6 +674,15 @@ export async function getCropIdToTermMap(farm) {
  * @returns an array of all of taxonomy terms representing tray sizes.
  */
 export async function getTraySizes(farm) {
+  if (global_tray_sizes) {
+    return global_tray_sizes;
+  }
+
+  if (libSessionStorage.getItem('tray_sizes') != null) {
+    global_tray_sizes = JSON.parse(libSessionStorage.getItem('tray_sizes'));
+    return global_tray_sizes;
+  }
+
   const traySizes = await farm.term.fetch({
     filter: {
       type: 'taxonomy_term--tray_size',
@@ -507,7 +700,9 @@ export async function getTraySizes(farm) {
     return size1 - size2;
   });
 
-  return traySizes.data;
+  libSessionStorage.setItem('tray_sizes', JSON.stringify(traySizes.data));
+  global_tray_sizes = traySizes.data;
+  return global_tray_sizes;
 }
 
 /**
@@ -544,6 +739,31 @@ export async function getTraySizeIdToTermMap(farm) {
   return map;
 }
 
+// Used to cache result of the getUnits function.
+var global_units = null;
+
+/**
+ * Clear the cached results from prior calls to the `getUnits` function.
+ * This is useful when an action may change the users that exist in the
+ * system
+ */
+export function clearCachedUnits() {
+  global_units = null;
+  libSessionStorage.removeItem('units');
+}
+
+/**
+ * @private
+ *
+ * Get the `global_units` object.  This is useful for testing to ensure
+ * that the global is set by the appropriate functions.
+ *
+ * @returns the `global_units` object
+ */
+export function getGlobalUnits() {
+  return global_units;
+}
+
 /**
  * Get taxonomy term objects for all of the units.
  * These are the taxonomy terms of type `taxonomy_term--unit`.
@@ -558,6 +778,15 @@ export async function getTraySizeIdToTermMap(farm) {
  * @returns an array of all of taxonomy terms representing units.
  */
 export async function getUnits(farm) {
+  if (global_units) {
+    return global_units;
+  }
+
+  if (libSessionStorage.getItem('units') != null) {
+    global_units = JSON.parse(libSessionStorage.getItem('units'));
+    return global_units;
+  }
+
   const units = await farm.term.fetch({
     filter: {
       type: 'taxonomy_term--unit',
@@ -573,7 +802,9 @@ export async function getUnits(farm) {
     o1.attributes.name.localeCompare(o2.attributes.name)
   );
 
-  return units.data;
+  libSessionStorage.setItem('units', JSON.stringify(units.data));
+  global_units = units.data;
+  return global_units;
 }
 
 /**
@@ -610,6 +841,31 @@ export async function getUnitIdToTermMap(farm) {
   return map;
 }
 
+// Used to cache result of the getLogCategories function.
+var global_log_categories = null;
+
+/**
+ * Clear the cached results from prior calls to the `getLogCategories` function.
+ * This is useful when an action may change the users that exist in the
+ * system
+ */
+export function clearCachedLogCategories() {
+  global_log_categories = null;
+  libSessionStorage.removeItem('log_categories');
+}
+
+/**
+ * @private
+ *
+ * Get the `global_units` object.  This is useful for testing to ensure
+ * that the global is set by the appropriate functions.
+ *
+ * @returns the `global_units` object
+ */
+export function getGlobalLogCategories() {
+  return global_log_categories;
+}
+
 /**
  * Get taxonomy term objects for all of the log categories.
  * These are the taxonomy terms of type `taxonomy_term--log_category`.
@@ -624,6 +880,17 @@ export async function getUnitIdToTermMap(farm) {
  * @returns an array of all of taxonomy terms representing log categories.
  */
 export async function getLogCategories(farm) {
+  if (global_log_categories) {
+    return global_log_categories;
+  }
+
+  if (libSessionStorage.getItem('log_categories') != null) {
+    global_log_categories = JSON.parse(
+      libSessionStorage.getItem('log_categories')
+    );
+    return global_log_categories;
+  }
+
   const categories = await farm.term.fetch({
     filter: {
       type: 'taxonomy_term--log_category',
@@ -639,7 +906,9 @@ export async function getLogCategories(farm) {
     o1.attributes.name.localeCompare(o2.attributes.name)
   );
 
-  return categories.data;
+  libSessionStorage.setItem('log_categories', JSON.stringify(categories.data));
+  global_log_categories = categories.data;
+  return global_log_categories;
 }
 
 /**
