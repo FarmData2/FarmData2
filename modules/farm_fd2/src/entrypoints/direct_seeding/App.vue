30b1c6ea54fa10a84f7ee03b4f8f876b9e903e2f
Grant Braught <braught@dickinson.edu>
2024-01-01 13:47:54 -0500

    feat(fd2): adds template for direct seeding entry point

---
+++ modules/farm_fd2/src/entrypoints/direct_seeding/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1 data-cy="title">Template for direct_seeding Entry Point</h1>
+  <p>
+    This uses the dayjs import: <span>{{ formatDate }}</span>
+  </p>
+  <p>
+    This uses the lib import: <span>{{ callLib }}</span>
+  </p>
+
+  <div
+    data-cy="page-loaded"
+    v-show="false"
+  >
+    {{ pageDoneLoading }}
+  </div>
+</template>
+
+<script>
+import dayjs from 'dayjs';
+import * as lib from './lib';
+
+export default {
+  data() {
+    return {
+      date: dayjs(),
+      createdCount: 0,
+    };
+  },
+  computed: {
+    formatDate() {
+      return this.date.format('YYYY-MM-DD');
+    },
+    callLib() {
+      return lib.placeholder();
+    },
+    pageDoneLoading() {
+      return this.createdCount == 1;
+    },
+  },
+  created() {
+    this.createdCount++;
+  },
+};
+</script>
---
+++ modules/farm_fd2/src/entrypoints/direct_seeding/lib.sample.unit.cy.js
@@ -0,0 +1,7 @@
+import * as lib from './lib.js';
+
+describe('Add tests for the lib.js file here.', () => {
+  it('Placeholder test', () => {
+    expect(lib.placeholder()).to.equal(1);
+  });
+});
