40378ac7bea0dd5ba008b8c66c378d29c8f9964d
Grant Braught <braught@dickinson.edu>
2024-01-09 16:15:07 -0500

    feat: adds equipment selector

---
+++ components/EquipmentSelector/EquipmentSelector.events.comp.cy.js
@@ -0,0 +1,37 @@
+import EquipmentSelector from '@comps/EquipmentSelector/EquipmentSelector.vue';
+
+describe('Test the EquipmentSelector component events', () => {
+  beforeEach(() => {
+    cy.restoreLocalStorage();
+    cy.restoreSessionStorage();
+  });
+
+  afterEach(() => {
+    cy.saveLocalStorage();
+    cy.saveSessionStorage();
+  });
+
+  it('Emits "valid" when component has been created', () => {
+    /*
+     * See `components/README.md` for information about component testing.
+     * See other components in the `components/` directory for examples.
+     */
+
+    const readySpy = cy.spy().as('readySpy');
+    const validSpy = cy.spy().as('validSpy');
+
+    cy.mount(EquipmentSelector, {
+      props: {
+        onReady: readySpy,
+        onValid: validSpy,
+      },
+    });
+
+    cy.get('@readySpy')
+      .should('have.been.calledOnce')
+      .then(() => {
+        cy.get('@validSpy').should('have.been.calledOnce');
+        cy.get('@validSpy').should('have.been.calledWith', false);
+      });
+  });
+});
