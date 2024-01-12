b91e90eb18bfccc2d8ea52c9eed63b809114bff3
Grant Braught <braught@dickinson.edu>
2024-01-12 08:38:40 -0500

    checkpoint

--- components/EquipmentSelector/EquipmentSelector.vue
+++ components/EquipmentSelector/EquipmentSelector.vue
@@ -1,40 +1,62 @@
 <template>
-  <div>
-    <SelectorBase
-      id="equipment-selector"
-      data-cy="equipment-selector"
-      label="Equipment"
-      invalidFeedbackText="Equipment must be selected"
-      v-bind:addOptionUrl="addEquipmentUrl"
-      v-bind:options="equipmentList"
-      v-bind:required="required"
-      v-bind:selected="selected"
-      v-bind:showValidityStyling="showValidityStyling"
-      v-on:update:selected="handleUpdateSelected($event)"
-      v-on:valid="handleValid($event)"
-    />
-  </div>
+  <BAccordion
+    flush
+    id="equipment-accordion"
+    data-cy="equipment-accordion"
+  >
+    <BAccordionItem title="Equipment">
+      <SelectorBase
+        v-bind:id="'equipment-selector-' + (i + 1)"
+        v-bind:data-cy="'equipment-selector-' + (i + 1)"
+        v-for="(line, i) in [...selected, '']"
+        invalidFeedbackText="Equipment must be selected"
+        v-bind:label="String(i + 1)"
+        v-bind:key="i"
+        v-bind:addOptionUrl="addEquipmentUrl(i)"
+        v-bind:options="equipmentList"
+        v-bind:required="isRequired(i)"
+        v-bind:selected="selected[i]"
+        v-bind:showValidityStyling="showValidityStyling"
+        v-on:update:selected="handleUpdateSelected($event, i)"
+        v-on:valid="handleValid($event, i)"
+      />
+    </BAccordionItem>
+  </BAccordion>
 </template>
 
 <script>
 import SelectorBase from '@comps/SelectorBase/SelectorBase.vue';
-//import * as farmosUtil from '@libs/farmosUtil/farmosUtil.js';
+import * as farmosUtil from '@libs/farmosUtil/farmosUtil.js';
 
 /**
- * A new component.
+ * A component for selecting equipment that was used for an activity
+ * (e.g. a direct seeding). The user can select multiple pieces of
+ * equipment as necessary.  The component appears in an accordion that
+ * can be opened and closed.  When opened it initially displays a single
+ * dropdown for selecting a piece of equipment.  When a piece of equipment
+ * is selected another dropdown appears to allow the user to select
+ * another piece of equipment.
  *
  * ## Usage Example
  *
  * ```html
- * Add example of how to add this component to a template.
- * See the other components in the `components` directory for examples.
+ * <EquipmentSelector
+ *   id="seeding-equipment"
+ *   data-cy="seeding-equipment"
+ *   v-model:selected="form.equipment"
+ *   v-bind:showValidityStyling="validity.show"
+ *   v-on:valid="validity.equipment = $event"
+ *   v-on:ready="createdCount++"
+ *  v-on:error="(msg) => showErrorToast('Network Error', msg)"
+ * />
  * ```
  *
  * ## `data-cy` Attributes
  *
- * Attribute Name        | Description
- * ----------------------| -----------
- * `attr-value`          | identify element with the `data-cy="attr-value"`
+ * Attribute Name         | Description
+ * -----------------------| -----------
+ * `equipment-accordion`  | The accordion that contains the equipment selector.
+ * `equipment-selector-i` | The `SelectorBase` component labeled `i:` (e.g. `1:`)
  */
 export default {
   name: 'EquipmentSelector',
@@ -42,21 +64,22 @@ export default {
   emits: ['error', 'ready', 'update:selected', 'valid'],
   props: {
     /**
-     * Whether a value for the input element is required or not.
+     * Whether an equipment selection is required or not. If an
+     * equipment selection is required then at least one piece of
+     * equipment must be selected.
      */
     required: {
       type: Boolean,
       default: false,
     },
-
-    /* TODO: NEED TO KNOW HOW TO SELECT MULTIPLE ELEMENTS. Is it an array? */
     /**
-     * The name of the selected equipment.
+     * An array of Strings indicating the names of the equipment that is selected on each line.
+     *
      * This prop is watched and changes are relayed to the component's internal state.
      */
     selected: {
       type: Array,
-      default: null,
+      default: () => [],
     },
     /**
      * Whether validity styling should appear on input elements.
@@ -68,49 +91,79 @@ export default {
   },
   data() {
     return {
-      equipmentList: ['A', 'B', 'C'],
-      canCreateEquipment: false,
+      selectedEquipment: this.selected,
+      valid: [],
+      equipmentList: [],
+      canCreateEquipment: true,
     };
   },
-  computed: {
-    addEquipmentUrl() {
-      if (this.canCreateEquipment) {
+  computed: {},
+  methods: {
+    addEquipmentUrl(i) {
+      if (i == this.selectedEquipment.length && this.canCreateEquipment) {
         return '/asset/add/equipment';
       } else {
         return null;
       }
     },
-  },
-  methods: {
-    handleUpdateSelected(event) {
-      /* TODO: Does this emit an array????? */
+    isRequired(i) {
+      return this.required && (i == 0 || i < this.selectedEquipment.length - 1);
+    },
+    handleUpdateSelected(event, i) {
+      /*
+       * Skip events that are a result of the page setting the
+       * selected prop to [] which generates a selected event for
+       * the blank ('') first option in SelectorBase.
+       */
+      if (event != '') {
+        this.selectedEquipment[i] = event;
+
+        /**
+         * The selected equipment has changed.
+         * @property {Array<String>} event the names of the newly selected equipment.
+         */
+        this.$emit('update:selected', this.selectedEquipment);
+      }
+    },
+    handleValid(event, i) {
+      this.valid[i] = event;
+
+      let allValid = false;
+      // check if all but the last equipment line is valid
+      if (this.selectedEquipment.length == 1) {
+        allValid = this.valid[0];
+      } else {
+        allValid = this.valid
+          .slice(0, this.selectedEquipment.length - 1)
+          .every((valid) => valid === true);
+      }
 
       /**
-       * The selected equipment has changed.
-       * @property {Array<String>} event the names of the newly selected equipment.
+       * The validity of the selected equipment changed.
+       * @property {boolean} event whether the selected equipment is valid or not.
        */
-      this.$emit('update:selected', event);
-    },
-    handleValid(event) {
-      /**
-       * The validity of the selected tray size has changed.
-       * @property {boolean} event whether the selected tray size is valid or not.
-       */
-      this.$emit('valid', event);
+      this.$emit('valid', allValid);
     },
   },
   watch: {
-    isValid() {
-      /**
-       * The validity of the component has changed.  Also emitted when the component is created.
-       * @property {Boolean} valid `true` if the component's value is valid; `false` if it is invalid.
-       */
-      this.$emit('valid', this.isValid);
+    selected() {
+      this.selectedEquipment = this.selected;
     },
   },
   created() {
-    //Emit the initial valid state of the component's value.
-    this.$emit('valid', this.isValid);
+    const canCreate = farmosUtil.checkPermission('create-equipment-asset');
+    const equipmentMap = farmosUtil.getEquipmentNameToAssetMap();
+
+    Promise.all([canCreate, equipmentMap])
+      .then(([canCreate, equipmentMap]) => {
+        this.canCreateEquipment = canCreate;
+        this.equipmentList = Array.from(equipmentMap.keys());
+      })
+      .catch((err) => {
+        console.error('EquipmentSelector: Error fetching equipment.');
+        console.error(err);
+        this.$emit('error', 'Unable to fetch locations.');
+      });
 
     /**
      * The component is ready for use.
--- modules/farm_fd2/src/entrypoints/direct_seeding/App.vue
+++ modules/farm_fd2/src/entrypoints/direct_seeding/App.vue
@@ -69,19 +69,7 @@
           v-on:ready="createdCount++"
         />
 
-        <SelectorBase
-          id="seeding-rows"
-          data-cy="seeding-rows"
-          label="Rows/Bed"
-          invalidFeedbackText="A number of rows is required"
-          v-model:selected="form.rowsPerBed"
-          v-bind:options="rowValues"
-          v-bind:required="true"
-          v-bind:showValidityStyling="validity.show"
-          v-on:valid="validity.rowsPerBed = $event"
-          v-on:ready="createdCount++"
-        />
-
+        <!-- Bed Width -->
         <NumericInput
           id="seeding-bed-width"
           data-cy="seeding-bed-width"
@@ -97,9 +85,23 @@
           v-on:ready="createdCount++"
         />
 
+        <!-- Rows/Bed -->
+        <SelectorBase
+          id="seeding-rows"
+          data-cy="seeding-rows"
+          label="Rows/Bed"
+          invalidFeedbackText="A number of rows is required"
+          v-model:selected="form.rowsPerBed"
+          v-bind:options="rowValues"
+          v-bind:required="true"
+          v-bind:showValidityStyling="validity.show"
+          v-on:valid="validity.rowsPerBed = $event"
+          v-on:ready="createdCount++"
+        />
+
         <hr />
 
-        <!-- Equipment Selection -->
+        <!-- Equipment Selector-->
         <EquipmentSelector
           id="seeding-equipment"
           data-cy="seeding-equipment"
@@ -109,6 +111,8 @@
           v-on:ready="createdCount++"
           v-on:error="(msg) => showErrorToast('Network Error', msg)"
         />
+
+        <hr />
 
         <p>More stuff here</p>
 
@@ -179,7 +183,7 @@ export default {
         bedFeet: 100,
         rowsPerBed: '1',
         bedWidth: 60,
-        equipment: null,
+        equipment: [],
         comment: null,
       },
       validity: {
@@ -214,6 +218,8 @@ export default {
           'success'
         );
 
+        console.dir(this.form);
+
         this.reset(true);
       } else {
         this.enableSubmit = false;
@@ -228,6 +234,7 @@ export default {
         this.form.bedFeet = 100;
         this.form.rowsPerBed = '1';
         this.form.bedWidth = 60;
+        this.form.equipment = [];
       }
 
       this.form.cropName = null;
@@ -237,7 +244,7 @@ export default {
   },
   computed: {
     pageDoneLoading() {
-      return this.createdCount == 9;
+      return this.createdCount == 10;
     },
   },
   watch: {
@@ -267,8 +274,13 @@ export default {
 #seeding-crop-name,
 #seeding-location-name,
 #seeding-bed-feet,
-#seeding-rows {
+#seeding-bed-width,
+#seeding-equipment-selector {
   margin-bottom: 8px;
+}
+
+#seeding-equipment {
+  margin-top: 3px;
 }
 
 #seeding-comment {
