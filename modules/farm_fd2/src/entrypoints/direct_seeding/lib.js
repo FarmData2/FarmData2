5ce01e53faf75ca2cf28482d6b9a41ab4624ac12
Grant Braught <braught@dickinson.edu>
2024-01-15 17:37:59 -0500

    chore(fd2): adds and tests submission code for direct seedings

--- modules/farm_fd2/src/entrypoints/direct_seeding/lib.js
+++ modules/farm_fd2/src/entrypoints/direct_seeding/lib.js
@@ -1,9 +1,8 @@
 import * as farmosUtil from '@libs/farmosUtil/farmosUtil';
-import dayjs from 'dayjs';
 
 /**
  * Create the farmOS records (asset, quantities and log) to represent
- * a directseeding.
+ * a direct seeding.
  *
  * @param {Object} formData the form data from the direct seeding form.
  * @returns {Promise} a promise that resolves when the records are successfully created.
@@ -19,13 +18,135 @@ import dayjs from 'dayjs';
  *   equipment: [ {asset--equipment} ],
  *   depth: {quantity--standard},
  *   speed: {quantity--standard},
+ *   activityLog: {log--activity},
  * }
  * ```
  * @throws {Error} if an error occurs while creating the farmOS records.
  */
 export async function submitForm(formData) {
   let plantAsset = null;
-  let traysQuantity = null;
-  let traySizeQuantity = null;
-  let seedsQuantity = null;
+  let bedFeetQuantity = null;
+  let rowsPerBedQuantity = null;
+  let bedWidthQuantity = null;
   let seedingLog = null;
+  let equipmentAssets = [];
+  let depthQuantity = null;
+  let speedQuantity = null;
+  let activityLog = null;
+
+  try {
+    const assetName = formData.seedingDate + '_ds_' + formData.cropName;
+
+    plantAsset = await farmosUtil.createPlantAsset(
+      assetName,
+      formData.cropName,
+      formData.comment
+    );
+
+    bedFeetQuantity = await farmosUtil.createStandardQuantity(
+      'length',
+      formData.bedFeet,
+      'Bed Feet',
+      'FEET'
+    );
+
+    rowsPerBedQuantity = await farmosUtil.createStandardQuantity(
+      'ratio',
+      formData.rowsPerBed,
+      'Rows/Bed',
+      'ROWS/BED'
+    );
+
+    bedWidthQuantity = await farmosUtil.createStandardQuantity(
+      'length',
+      formData.bedWidth,
+      'Bed Width',
+      'INCHES'
+    );
+
+    seedingLog = await farmosUtil.createSeedingLog(
+      formData.seedingDate,
+      formData.locationName,
+      'seeding_direct',
+      plantAsset,
+      [bedFeetQuantity, rowsPerBedQuantity, bedWidthQuantity]
+    );
+
+    depthQuantity = await farmosUtil.createStandardQuantity(
+      'length',
+      formData.depth,
+      'Depth',
+      'INCHES'
+    );
+
+    speedQuantity = await farmosUtil.createStandardQuantity(
+      'rate',
+      formData.speed,
+      'Speed',
+      'MPH'
+    );
+
+    const equipmentMap = await farmosUtil.getEquipmentNameToAssetMap();
+    for (const equipmentName of formData.equipment) {
+      equipmentAssets.push(equipmentMap.get(equipmentName));
+    }
+
+    activityLog = await farmosUtil.createSoilDisturbanceActivityLog(
+      formData.seedingDate,
+      formData.locationName,
+      'activity_soil_disturbance_tillage',
+      plantAsset,
+      [depthQuantity, speedQuantity],
+      equipmentAssets
+    );
+
+    return {
+      plantAsset: plantAsset,
+      bedFeetQuantity: bedFeetQuantity,
+      rowsPerBedQuantity: rowsPerBedQuantity,
+      bedWidthQuantity: bedWidthQuantity,
+      seedingLog: seedingLog,
+      depthQuantity: depthQuantity,
+      speedQuantity: speedQuantity,
+      equipment: equipmentAssets,
+      activityLog: activityLog,
+    };
+  } catch (error) {
+    console.log('DirectSeeding: \n' + error.message);
+    console.log(error);
+
+    if (seedingLog) {
+      await farmosUtil.deleteSeedingLog(seedingLog.id);
+    }
+
+    if (rowsPerBedQuantity) {
+      await farmosUtil.deleteStandardQuantity(rowsPerBedQuantity.id);
+    }
+
+    if (bedWidthQuantity) {
+      await farmosUtil.deleteStandardQuantity(bedWidthQuantity.id);
+    }
+
+    if (bedFeetQuantity) {
+      await farmosUtil.deleteStandardQuantity(bedFeetQuantity.id);
+    }
+
+    if (activityLog) {
+      // delete this.
+    }
+
+    if (depthQuantity) {
+      await farmosUtil.deleteStandardQuantity(depthQuantity.id);
+    }
+
+    if (speedQuantity) {
+      await farmosUtil.deleteStandardQuantity(speedQuantity.id);
+    }
+
+    if (plantAsset) {
+      await farmosUtil.deletePlantAsset(plantAsset.id);
+    }
+
+    throw Error('Error creating direct seeding.', error);
+  }
+}
