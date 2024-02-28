da5e8151e65e7657d820a3c963abef624596c783
Grant Braught <braught@dickinson.edu>
2024-02-27 20:45:57 -0500

    feat(examples): adds entry point to collect component examples

---
+++ modules/farm_fd2_examples/src/entrypoints/component_examples/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <h2>Component Examples</h2>
+
+  <p>A set of examples illustrating the use of the FarmData2 components.</p>
+  <ul>
+    <li><a href="">ComponentName</a>: Component for blah blah.</li>
+  </ul>
+
+  <div
+    data-cy="page-loaded"
+    v-show="false"
+  >
+    {{ pageDoneLoading }}
+  </div>
+
+  <br />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      createdCount: 0,
+    };
+  },
+  computed: {
+    pageDoneLoading() {
+      return this.createdCount == 1;
+    },
+  },
+  created() {
+    this.createdCount++;
+  },
+};
+</script>
+
+<style>
+@import url('@css/fd2-mobile.css');
+</style>
