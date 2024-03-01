85cceff0ce8463086bf74f18d6bc114f37bfba99
Grant Braught <braught@dickinson.edu>
2024-03-01 11:41:28 -0500

    feat(examples): starts the PickerBase example

---
+++ modules/farm_fd2_examples/src/entrypoints/picker_base/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <p>
+    PickerBase is a component that allows the user to pick multiple items using
+    checkboxes.
+  </p>
+
+  <hr />
+  <PickerBase
+    v-if="picks.length > 0"
+    id="bed-picker"
+    data-cy="bed-picker"
+    label="Options"
+    v-bind:options="picks"
+  />
+  <hr />
+
+  <div
+    data-cy="page-loaded"
+    v-show="false"
+  >
+    {{ pageDoneLoading }}
+  </div>
+</template>
+
+<script>
+import PickerBase from '@comps/PickerBase/PickerBase.vue';
+
+export default {
+  components: {
+    PickerBase,
+  },
+  data() {
+    return {
+      picks: ['A', 'B', 'C'],
+      createdCount: 0,
+    };
+  },
+  computed: {
+    pageDoneLoading() {
+      return this.createdCount == 1;
+    },
+  },
+  created() {
+    this.createdCount++;
+  },
+};
+</script>
