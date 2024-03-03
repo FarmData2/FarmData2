f73aee8552525f22db9f7cd53d379b26b8b1d248
Grant Braught <braught@dickinson.edu>
2024-03-03 16:04:23 -0500

    test(comp): switches valid to emtpy in PickerBase tests

--- components/PickerBase/PickerBase.styling.comp.cy.js
+++ components/PickerBase/PickerBase.styling.comp.cy.js
@@ -14,24 +14,25 @@ describe('Test the PickerBase component styling', () => {
   /*
    * There are 8 possibilities for styling...
    *
-   * required   showValidityStyling    isValid   Tested by Test
-   * false      false                  false     5. Not required, Not showing, not valid
-   * false      false                  true      6. Not required, Not showing, valid
-   * false      true                   false     7. Not required, Showing, not valid
-   * false      true                   true      8. Not required, Showing, valid
-   * true       false                  false     5. Not required, Not showing, not valid
-   * true       false                  true      6. Not required, Not showing, valid
-   * true       true                   false     7. Not required, Showing, not valid
-   * true       true                   true      8. Not required, Showing, valid
+   * required   showValidityStyling    isEmpty   Tested by Test
+   * false      false                  false     5. Not required, Not showing, not empty
+   * false      false                  true      6. Not required, Not showing, empty
+   * false      true                   false     7. Not required, Showing, not empty
+   * false      true                   true      8. Not required, Showing, empty
+   * true       false                  false     5. Not required, Not showing, not empty
+   * true       false                  true      6. Not required, Not showing, empty
+   * true       true                   false     7. Not required, Showing, not empty
+   * true       true                   true      8. Not required, Showing, empty
    */
 
-  it('1. Not required, Not showing, not valid', () => {
+  it('1. Not required, Not showing, not empty', () => {
     const readySpy = cy.spy().as('readySpy');
 
     cy.mount(PickerBase, {
       props: {
         required: false,
         showValidityStyling: false,
+        picked: ['Option 1'],
         label: 'Picker',
         options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
         invalidFeedbackText: 'Invalid feedback text.',
@@ -54,43 +55,14 @@ describe('Test the PickerBase component styling', () => {
       });
   });
 
-  it('2. Not required, Not showing, valid', () => {
+  it('2. Not required, Not showing, empty', () => {
     const readySpy = cy.spy().as('readySpy');
 
     cy.mount(PickerBase, {
       props: {
         required: false,
         showValidityStyling: false,
-        label: 'Picker',
-        options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
-        picked: ['Option 1'],
-        invalidFeedbackText: 'Invalid feedback text.',
-        onReady: readySpy,
-      },
-    });
-
-    cy.get('@readySpy')
-      .should('have.been.calledOnce')
-      .then(() => {
-        cy.get('[data-cy="picker-options"]')
-          .find('input')
-          .eq(0)
-          .should('not.have.class', 'is-valid');
-        cy.get('[data-cy="picker-options"]')
-          .find('input')
-          .eq(0)
-          .should('not.have.class', 'is-invalid');
-        cy.get('[data-cy="picker-invalid-feedback"]').should('not.be.visible');
-      });
-  });
-
-  it('3. Not required, Showing, not valid', () => {
-    const readySpy = cy.spy().as('readySpy');
-
-    cy.mount(PickerBase, {
-      props: {
-        required: false,
-        showValidityStyling: true,
+        picked: [],
         label: 'Picker',
         options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
         invalidFeedbackText: 'Invalid feedback text.',
@@ -113,16 +85,16 @@ describe('Test the PickerBase component styling', () => {
       });
   });
 
-  it('4. Not required, Showing, valid', () => {
+  it('3. Not required, Showing, not empty', () => {
     const readySpy = cy.spy().as('readySpy');
 
     cy.mount(PickerBase, {
       props: {
         required: false,
         showValidityStyling: true,
+        picked: ['Option 1'],
         label: 'Picker',
         options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
-        picked: ['Option 1'],
         invalidFeedbackText: 'Invalid feedback text.',
         onReady: readySpy,
       },
@@ -143,13 +115,14 @@ describe('Test the PickerBase component styling', () => {
       });
   });
 
-  it('5. Required, Not showing, not valid', () => {
+  it('4. Not required, Showing, empty', () => {
     const readySpy = cy.spy().as('readySpy');
 
     cy.mount(PickerBase, {
       props: {
-        required: true,
-        showValidityStyling: false,
+        required: false,
+        showValidityStyling: true,
+        picked: [],
         label: 'Picker',
         options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
         invalidFeedbackText: 'Invalid feedback text.',
@@ -172,16 +145,16 @@ describe('Test the PickerBase component styling', () => {
       });
   });
 
-  it('6. Required, Not showing, valid', () => {
+  it('5. Required, Not showing, not empty', () => {
     const readySpy = cy.spy().as('readySpy');
 
     cy.mount(PickerBase, {
       props: {
         required: true,
         showValidityStyling: false,
-        label: 'Picker',
-        options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
         picked: ['Option 1'],
+        label: 'Picker',
+        options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
         invalidFeedbackText: 'Invalid feedback text.',
         onReady: readySpy,
       },
@@ -202,13 +175,74 @@ describe('Test the PickerBase component styling', () => {
       });
   });
 
-  it('7. Required, Showing, not valid', () => {
+  it('6. Required, Not showing, empty', () => {
+    const readySpy = cy.spy().as('readySpy');
+
+    cy.mount(PickerBase, {
+      props: {
+        required: true,
+        showValidityStyling: false,
+        picked: [],
+        label: 'Picker',
+        options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+        invalidFeedbackText: 'Invalid feedback text.',
+        onReady: readySpy,
+      },
+    });
+
+    cy.get('@readySpy')
+      .should('have.been.calledOnce')
+      .then(() => {
+        cy.get('[data-cy="picker-options"]')
+          .find('input')
+          .eq(0)
+          .should('not.have.class', 'is-valid');
+        cy.get('[data-cy="picker-options"]')
+          .find('input')
+          .eq(0)
+          .should('not.have.class', 'is-invalid');
+        cy.get('[data-cy="picker-invalid-feedback"]').should('not.be.visible');
+      });
+  });
+
+  it('7. Required, Showing, not empty', () => {
     const readySpy = cy.spy().as('readySpy');
 
     cy.mount(PickerBase, {
       props: {
         required: true,
         showValidityStyling: true,
+        picked: ['Option 1'],
+        label: 'Picker',
+        options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+        invalidFeedbackText: 'Invalid feedback text.',
+        onReady: readySpy,
+      },
+    });
+
+    cy.get('@readySpy')
+      .should('have.been.calledOnce')
+      .then(() => {
+        cy.get('[data-cy="picker-options"]')
+          .find('input')
+          .eq(0)
+          .should('have.class', 'is-valid');
+        cy.get('[data-cy="picker-options"]')
+          .find('input')
+          .eq(0)
+          .should('not.have.class', 'is-invalid');
+        cy.get('[data-cy="picker-invalid-feedback"]').should('not.be.visible');
+      });
+  });
+
+  it('8. Required, Showing, empty', () => {
+    const readySpy = cy.spy().as('readySpy');
+
+    cy.mount(PickerBase, {
+      props: {
+        required: true,
+        showValidityStyling: true,
+        picked: [],
         label: 'Picker',
         options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
         invalidFeedbackText: 'Invalid feedback text.',
@@ -228,36 +262,6 @@ describe('Test the PickerBase component styling', () => {
           .eq(0)
           .should('have.class', 'is-invalid');
         cy.get('[data-cy="picker-invalid-feedback"]').should('be.visible');
-      });
-  });
-
-  it('8. Required, Showing, valid', () => {
-    const readySpy = cy.spy().as('readySpy');
-
-    cy.mount(PickerBase, {
-      props: {
-        required: true,
-        showValidityStyling: true,
-        label: 'Picker',
-        options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
-        picked: ['Option 1'],
-        invalidFeedbackText: 'Invalid feedback text.',
-        onReady: readySpy,
-      },
-    });
-
-    cy.get('@readySpy')
-      .should('have.been.calledOnce')
-      .then(() => {
-        cy.get('[data-cy="picker-options"]')
-          .find('input')
-          .eq(0)
-          .should('have.class', 'is-valid');
-        cy.get('[data-cy="picker-options"]')
-          .find('input')
-          .eq(0)
-          .should('not.have.class', 'is-invalid');
-        cy.get('[data-cy="picker-invalid-feedback"]').should('not.be.visible');
       });
   });
 });
