a4293093ecae52c46f94db7bd0411f993c660d0c
Grant Braught <braught@dickinson.edu>
2024-03-28 10:15:15 -0400

    checkpoint

--- modules/farm_fd2/src/entrypoints/transplanting/App.vue
+++ modules/farm_fd2/src/entrypoints/transplanting/App.vue
@@ -1,12 +1,57 @@
 <template>
-  <h1 data-cy="title">Template for transplanting Entry Point</h1>
-  <p>
-    This uses the dayjs import: <span>{{ formatDate }}</span>
-  </p>
-  <p>
-    This uses the lib import: <span>{{ callLib }}</span>
-  </p>
+  <div
+    id="transplanting"
+    data-cy="transplanting"
+  >
+    <BToaster
+      id="transplanting-toaster"
+      data-cy="transplanting-toaster"
+    />
+    <BCard
+      id="transplanting-card"
+      data-cy="transplanting-card"
+      bg-variant="light"
+      header-tag="header"
+    >
+      <template #header>
+        <h2
+          id="transplanting-header"
+          data-cy="transplanting-header"
+          class="text-center"
+        >
+          Transplanting
+        </h2>
+      </template>
 
+      <BForm
+        id="transplanting-form"
+        data-cy="transplanting-form"
+      >
+        <!-- Crop Filter -->
+        <SelectorBase
+          id="transplanting-crop-filter"
+          data-cy="transplanting-crop-filter"
+          label="Crop"
+          v-model:selected="cropFilter"
+          v-bind:options="cropList"
+          v-bind:invalidFeedbackText="'Please select a crop.'"
+          v-bind:showValidityStyling="validity.show"
+          v-on:valid="validity.cropFilter = $event"
+          v-on:ready="createdCount++"
+        />
+
+        <!-- Tray Seeding Picklist -->
+      </BForm>
+    </BCard>
+
+    <div
+      id="page-loaded"
+      data-cy="page-loaded"
+      v-show="false"
+    >
+      {{ pageDoneLoading }}
+    </div>
+  </div>
   <div
     data-cy="page-loaded"
     v-show="false"
@@ -18,27 +63,39 @@
 <script>
 import dayjs from 'dayjs';
 import * as lib from './lib';
+import * as farmosUtil from '@libs/farmosUtil/farmosUtil.js';
+import SelectorBase from '@comps/SelectorBase/SelectorBase.vue';
 
 export default {
+  components: {
+    SelectorBase,
+  },
   data() {
     return {
-      date: dayjs(),
+      cropFilter: null,
+      cropList: [],
+      seedlingList: [],
+      form: {},
+      validity: {
+        show: false,
+      },
       createdCount: 0,
     };
   },
   computed: {
-    formatDate() {
-      return this.date.format('YYYY-MM-DD');
-    },
-    callLib() {
-      return lib.placeholder();
-    },
     pageDoneLoading() {
-      return this.createdCount == 1;
+      return this.createdCount == 2;
     },
   },
   created() {
-    this.createdCount++;
+    // Fetch list of seedlings that can be transplanted.
+    farmosUtil.getTraySeededCropNames().then((cropNames) => {
+      this.cropList = ['ALL', ...cropNames];
+      this.createdCount++;
+    })
+    .catch((error) => {
+      console.error('SeedlingSelector: Error fetching seedlings.');
+    });
   },
 };
 </script>
