2bdfb2db3b311969b284cfbcf38f4b59cca74bd4
Grant Braught <braught@dickinson.edu>
2024-04-02 17:25:54 -0400

    checkpoint

--- modules/farm_fd2/src/entrypoints/transplanting/App.vue
+++ modules/farm_fd2/src/entrypoints/transplanting/App.vue
@@ -27,35 +27,9 @@
         id="transplanting-form"
         data-cy="transplanting-form"
       >
-        <!-- Crop Filter -->
-        <SelectorBase
-          id="transplanting-crop-filter"
-          data-cy="transplanting-crop-filter"
-          label="Crop"
-          required
-          v-model:selected="cropFilter"
-          v-bind:options="cropList"
-          v-bind:invalidFeedbackText="'A crop must be selected.'"
-          v-bind:showValidityStyling="validity.show"
-          v-on:valid="validity.cropFilter = $event"
-          v-on:update:selected="cropFilterChanged($event)"
-          v-on:ready="createdCount++"
-        />
+        <!-- Transplanting Picklist -->
 
-        <!-- Tray Seeding Picklist -->
-        <PicklistBase
-          id="transplanting-picklist"
-          data-cy="transplanting-picklist"
-          v-bind:columns="columns"
-          v-bind:labels="labels"
-          v-bind:rows="sortedRows"
-          showAllButton
-          showInfoIcons
-          v-bind:picked="form.picked"
-          v-on:valid="(valid) => (validity.picked = valid)"
-          v-on:update:picked="form.picked = $event"
-          v-on:ready="createdCount++"
-        />
+          
       </BForm>
     </BCard>
 
@@ -79,32 +53,14 @@
 import dayjs from 'dayjs';
 import * as lib from './lib';
 import * as farmosUtil from '@libs/farmosUtil/farmosUtil.js';
-import SelectorBase from '@comps/SelectorBase/SelectorBase.vue';
-import PicklistBase from '@comps/PicklistBase/PicklistBase.vue';
+import TransplantingPicklist from '@comps/TransplantingPicklist/TransplantingPicklist.vue';
 
 export default {
   components: {
-    SelectorBase,
-    PicklistBase,
+    TransplantingPicklist,
   },
   data() {
     return {
-      cropFilter: null,
-      cropList: [],
-      columns: ['date', 'trays_location', 'tray_ratio'],
-      labels: {
-        date: 'Date',
-        user: 'User',
-        trays_location: 'Location',
-        asset_location: 'Transplanted Location',
-        tray_ratio: 'Trays',
-        tray_size: 'Tray Size',
-        seeds_per_cell: 'Seeds per Cell',
-        total_seeds: 'Total Seeds',
-        log_notes: 'Seeding Notes',
-        asset_notes: 'Plant Notes',
-      },
-      seedlingList: [],
       form: {
         picked: [],
       },
@@ -119,50 +75,16 @@ export default {
     pageDoneLoading() {
       return this.createdCount == 2;
     },
-    sortedRows() {
-      return this.seedlingList.sort((a, b) => {
-        return dayjs(a.date).isBefore(dayjs(b.date)) ? -1 : 1;
-      })
-    }
   },
   methods: {
-    cropFilterChanged(cropName) {
-      if (cropName) {
-        farmosUtil
-          .getSeedlings(cropName)
-          .then((seedlings) => {
-            this.seedlingList = seedlings;
-          })
-          .catch((error) => {
-            // TODO: Use toast here...
-            console.error('Transplanting: Error fetching seedlings.');
-          });
-      } else {
-        this.seedlingList = [];
-      }
-    },
+    
   },
   created() {
-    // Fetch list of seedlings that can be transplanted.
-    farmosUtil
-      .getTraySeededCropNames()
-      .then((cropNames) => {
-        this.cropList = cropNames;
-        this.createdCount++;
-      })
-      .catch((error) => {
-        // TODO: Use toast here...
-        console.error('Transplanting: Error fetching seedlings crop names.');
-      });
+    this.createdCount++;
   },
 };
 </script>
 
 <style>
 @import url('@css/fd2-mobile.css');
-
-#transplanting-crop-filter {
-  padding-top: 3px;
-  padding-bottom: 8px;
-}
 </style>
