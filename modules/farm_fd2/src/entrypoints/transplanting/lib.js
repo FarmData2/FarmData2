588337ce2f551376947d9431278c1b1af520f7ea
Grant Braught <braught@dickinson.edu>
2024-04-05 16:41:52 -0400

    checkpoint

--- modules/farm_fd2/src/entrypoints/transplanting/lib.js
+++ modules/farm_fd2/src/entrypoints/transplanting/lib.js
@@ -1,15 +1,36 @@
-/*
- * Add functions here so that they can be unit tested and
- * then used in the end point.
- *
- * This is typically used for the complicated logic involved
- * in creating farmOS records for a form.
- */
+import * as farmosUtil from '@libs/farmosUtil/farmosUtil';
 
 /**
- * Just a function here to ensure things work.
- * @returns 1
+ * Create the farmOS records (asset, quantities and log) to represent
+ * the transplanted trays.
+ *
+ * @param {Object} formData the form data from the transplanting form.
+ * @returns {Promise} a promise that resolves when the records are successfully created.
+ * The returned value is an object containing the asset, quantities and log that
+ * were sent to the server.  This object has the following properties:
+ * ```Javascript
+ * {
+ *   plantAsset: {asset--plant},
+ * 
+ *   TODO: FILL THIS IN LATER...
+ * }
+ * ```
+ * @throws {Error} if an error occurs while creating the farmOS records.
  */
-export function placeholder() {
-  return 1;
+export async function submitForm(formData) {
+  try {
+    // create the plant asset
+    // Asset from each row as a parent
+
+    // create the transplanting activity log
+    // Quantity from each row as inventory decrement
+
+    // archive the original plant asset if inventory == 0
+
+    // create the soil disturbance activity log
+  }
+  catch (error) {
+    // delete anything that was created
+    // throw an error.
+  }
 }
