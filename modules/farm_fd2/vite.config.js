5d34d44c4f8b05045b03f2800467e36098f981c2
Grant Braught <braught@dickinson.edu>
2024-05-05 16:56:15 -0400

    fix: builds single css file for each module (#246)
    **Pull Request Description**
    
    This PR changes the vite builds so that there is a single `css` file for
    each module. Vite was splitting some of the CSS into component specific
    files (e.g. `CropSelector/CropSelector.css`) Then to have this styling
    work within an entry point that `css` file would need to be added to the
    `*.libraries.yml` file so that Drupal makes it available to the page.
    This was complicated, and a bit unpredictable because it was unclear why
    or when vite would do this. So, instead a single `css` file is now
    created in `style/style.css` and that file is included in the
    `*.libraries.yml` for each entry point. That introduces a small
    inefficiency as all of the CSS is loaded on each entry point even if it
    is not used. But this should be a small overhead.
    
    Closes #202 
    
    ---
    
    **Licensing Certification**
    
    FarmData2 is a [Free Cultural
    Work](https://freedomdefined.org/Definition) and all accepted
    contributions are licensed as described in the LICENSE.md file. This
    requires that the contributor holds the rights to do so. By submitting
    this pull request **I certify that I satisfy the terms of the [Developer
    Certificate of Origin](https://developercertificate.org/)** for its
    contents.

--- modules/farm_fd2/vite.config.js
+++ modules/farm_fd2/vite.config.js
@@ -63,7 +63,7 @@ let viteConfig = {
   build: {
     outDir: '../../dist/farmdata2',
     emptyOutDir: true,
-    exclude: ['**/*.cy.js', '**/*.cy.comp.js', '**/*.cy.unit.js'],
+    cssCodeSplit: false,
     rollupOptions: {
       input: Object.fromEntries(
         glob.sync('modules/farm_fd2/src/entrypoints/*/*.html').map((dir) => {
@@ -74,19 +74,7 @@ let viteConfig = {
       output: {
         // Ensures that the entry point and css names are not hashed.
         entryFileNames: '[name]/[name].js',
-        assetFileNames: (assetInfo) => {
-          let name = assetInfo.name.split('/').at(0);
-          let ext = assetInfo.name.split('.').at(1);
-          if (ext === 'css') {
-            if (name.startsWith('_plugin-vue_')) {
-              return 'shared/index.css';
-            } else {
-              return '[name]/[name].css';
-            }
-          } else {
-            return 'shared/[name].[ext]';
-          }
-        },
+        assetFileNames: '[name]/[name].[ext]',
         chunkFileNames: '[name]/[name].js',
       },
     },
