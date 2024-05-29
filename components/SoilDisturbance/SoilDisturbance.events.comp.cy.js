cf079843ef00f1c1c8250456cef464cb6a06c459
sidlamsal <lamsals@dickinson.edu>
2024-05-29 15:00:21 -0400

    added test in SoilDisurbance.events to check that area is not effecting validity when not required

--- components/SoilDisturbance/SoilDisturbance.events.comp.cy.js
+++ components/SoilDisturbance/SoilDisturbance.events.comp.cy.js
@@ -155,6 +155,29 @@ describe('Test the SoilDisturbance component events', () => {
       });
   });
 
+  it('"valid" event ignores area if not included', () => {
+    const readySpy = cy.spy().as('readySpy');
+    const validSpy = cy.spy().as('validSpy');
+
+    cy.mount(SoilDisturbance, {
+      props: {
+        required: true,
+        area: null,
+        includeArea: false,
+        equipment: ['Tractor'],
+        onReady: readySpy,
+        onValid: validSpy,
+      },
+    });
+
+    cy.get('@readySpy')
+      .should('have.been.calledOnce')
+      .then(() => {
+        cy.get('@validSpy').should('have.been.calledOnce');
+        cy.get('@validSpy').should('have.been.calledWith', true);
+      });
+  });
+
   it('Changing area emits "valid" event correctly', () => {
     const readySpy = cy.spy().as('readySpy');
     const validSpy = cy.spy().as('validSpy');
