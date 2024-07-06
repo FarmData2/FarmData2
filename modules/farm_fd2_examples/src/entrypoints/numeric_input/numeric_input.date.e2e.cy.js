5c900058073b654ed86eee40d057a6eaccbcea00
sidlamsal <lamsals@dickinson.edu>
2024-07-06 18:53:44 -0400

    starter code

---
+++ modules/farm_fd2_examples/src/entrypoints/numeric_input/numeric_input.date.e2e.cy.js
@@ -0,0 +1,42 @@
+import dayjs from 'dayjs';
+
+describe('NumericInput Example: Date Component', () => {
+  beforeEach(() => {
+    cy.restoreLocalStorage();
+    cy.restoreSessionStorage();
+
+    cy.login('admin', 'admin');
+    cy.visit('fd2_examples/numeric_input/');
+
+    cy.waitForPage();
+  });
+
+  afterEach(() => {
+    cy.saveLocalStorage();
+    cy.saveSessionStorage();
+  });
+
+  it('Date exists, is visible, is enabled', () => {
+    cy.get('[data-cy="numeric-input-date"]').should('exist');
+    cy.get('[data-cy="date-input"]').should('be.visible').should('be.enabled');
+  });
+
+  it('Date props are correct', () => {
+    cy.get('[data-cy="date-required"]').should('be.visible');
+    cy.get('[data-cy="date-label"]').should('have.text', 'Date:');
+    cy.get('[data-cy="date-required"]').should('exist');
+    cy.get('[data-cy="date-input"]').should(
+      'have.value',
+      dayjs().format('YYYY-MM-DD')
+    );
+    cy.get('[data-cy="date-input"]').should('not.have.class', 'is-valid');
+    cy.get('[data-cy="date-input"]').should('not.have.class', 'is-invalid');
+  });
+
+  it('Date validity styling works', () => {
+    cy.get('[data-cy="date-input"]').clear();
+    cy.get('[data-cy="submit-button"]').click();
+    cy.get('[data-cy="date-input"]').should('not.have.class', 'is-valid');
+    cy.get('[data-cy="date-input"]').should('have.class', 'is-invalid');
+  });
+});
