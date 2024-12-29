14902d5778e23fc4ce42e1e7672b388a10044b6a
Grant Braught <braught@dickinson.edu>
2024-12-29 14:27:16 -0500

    adds heartbeat server to fd2dev

---
+++ .devcontainer/startHeartbeat.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+nohup docker exec fd2_dev sh -c \
+"inotifywait -mr \
+  --exclude 'node_modules' \
+  --exclude '/home/fd2dev/\..*' \
+  -e create,delete,modify /home/fd2dev | \
+  while read -r dirname events basename; do       
+    echo 'FD2 Dev Environment Heartbeat' | ncat 172.18.0.1 8888  
+  done"
