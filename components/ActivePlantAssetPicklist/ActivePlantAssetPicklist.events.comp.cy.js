0750c69c0b654f86fead61d3e6eaeda3955d1f71
Shahir-47 <ahmeds@dickinson.edu>
2025-02-17 21:33:23 -0500

    adds test for valid and error events

--- components/ActivePlantAssetPicklist/ActivePlantAssetPicklist.events.comp.cy.js
+++ components/ActivePlantAssetPicklist/ActivePlantAssetPicklist.events.comp.cy.js
@@ -11,12 +11,7 @@ describe('Test the ActivePlantAssetPicklist component events', () => {
     cy.saveSessionStorage();
   });
 
-  it('Emits "valid" when component has been created', () => {
-    /*
-     * See `components/README.md` for information about component testing.
-     * See other components in the `components/` directory for examples.
-     */
-
+  it('Test valid event propagated initially if required = false', () => {
     const readySpy = cy.spy().as('readySpy');
     const validSpy = cy.spy().as('validSpy');
 
@@ -31,7 +26,81 @@ describe('Test the ActivePlantAssetPicklist component events', () => {
       .should('have.been.calledOnce')
       .then(() => {
         cy.get('@validSpy').should('have.been.calledOnce');
+        cy.get('@validSpy').should('have.been.calledWith', true);
+      });
+  });
+
+  it('Test valid event propagated initially if required = true', () => {
+    const readySpy = cy.spy().as('readySpy');
+    const validSpy = cy.spy().as('validSpy');
+
+    cy.mount(ActivePlantAssetPicklist, {
+      props: {
+        required: true,
+        onReady: readySpy,
+        onValid: validSpy,
+      },
+    });
+
+    cy.get('@readySpy')
+      .should('have.been.calledOnce')
+      .then(() => {
+        cy.get('@validSpy').should('have.been.calledOnce');
         cy.get('@validSpy').should('have.been.calledWith', false);
       });
+  });
+
+  it('Test valid event propagated if crop picked if required = true', () => {
+    const readySpy = cy.spy().as('readySpy');
+    const validSpy = cy.spy().as('validSpy');
+
+    cy.mount(ActivePlantAssetPicklist, {
+      props: {
+        location: 'ALF',
+        required: true,
+        onReady: readySpy,
+        onValid: validSpy,
+      },
+    }).then(() => {
+      cy.get('@readySpy')
+        .should('have.been.calledOnce')
+        .then(() => {
+          cy.get('@validSpy').should('have.been.calledOnce');
+          cy.get('@validSpy').should('have.been.calledWith', false);
+
+          cy.get('[data-cy="picklist-checkbox-1"]').check();
+
+          cy.get('@validSpy').should('have.been.calledTwice');
+          cy.get('@validSpy').should('have.been.calledWith', true);
+
+          cy.get('[data-cy="picklist-checkbox-1"]').uncheck();
+
+          cy.get('@validSpy').should('have.been.calledThrice');
+          cy.get('@validSpy').should('have.been.calledWith', false);
+        });
+    });
+  });
+
+  it('Error event if unable to fetch plant assets', () => {
+    const readySpy = cy.spy().as('readySpy');
+    const errorSpy = cy.spy().as('errorSpy');
+
+    cy.intercept('GET', '**/api/**', {
+      forceNetworkError: true,
+    }).as('farmOSRequest');
+
+    cy.mount(ActivePlantAssetPicklist, {
+      props: {
+        location: 'ALF',
+        onReady: readySpy,
+        onError: errorSpy,
+      },
+    });
+
+    cy.wait('@farmOSRequest');
+
+    cy.get('@errorSpy')
+      .should('have.been.calledOnce')
+      .and('have.been.calledWith', 'Unable to fetch plant assets.');
   });
 });
