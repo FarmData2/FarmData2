5ad9f05e14dab607b25e146ea53df6e73b113519
braughtg <braught@dickinson.edu>
2025-03-05 11:21:26 -0500

    installs vale via npm and configures it in setup.bash

--- bin/setup.bash
+++ bin/setup.bash
@@ -40,9 +40,15 @@ echo "  Installed."
 
 echo ""
 
-echo "  Initializing vale linter..."
+echo "  Configuring vale linter..."
+if [ -f /usr/local/bin/vale ]; then
+  # Remove existing vale if it is left over from an old fd2dev docker container.
+  echo "fd2dev" | sudo rm /usr/local/bin/vale
+fi
+# Link to the version of vale installed by npm
+echo "fd2dev" | sudo ln -s "$REPO_ROOT_DIR"/node_modules/@vvago/vale/bin/vale /usr/local/bin/vale
 vale sync
-echo "  Initialized."
+echo "  Configured."
 
 echo "  Configuring git information..."
 echo "    The following information will be associated with GitHub commits"
