0d848b3379789703a9cefd9f5b5dbdf081c49e93
braughtg <braught@dickinson.edu>
2025-06-04 08:59:48 -0400

    WIP

--- bin/buildCompExListPage.bash
+++ bin/buildCompExListPage.bash
@@ -4,6 +4,12 @@
 # source code in the components directory.  This ensures that all of the components
 # are listed and linked in the component_examples entry point.
 
+# Get the path to the main repo directory.
+PWD="$(pwd)"
+SCRIPT_PATH=$(readlink -f "$0")                     # Path to this script.
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")                # Path to directory containing this script.
+REPO_ROOT_DIR=$(builtin cd "$SCRIPT_DIR/.." && pwd) # REPO root directory.
+
 INDEX_PAGE="$REPO_ROOT_DIR/modules/farm_fd2_examples/src/entrypoints/component_examples/App.vue"
 # Remove the temporary files that we are using.
 rm /var/tmp/App.vue 2> /dev/null
--- modules/farm_fd2_examples/src/entrypoints/component_examples/App.vue
+++ modules/farm_fd2_examples/src/entrypoints/component_examples/App.vue
@@ -3,87 +3,64 @@
   <p>A set of examples illustrating the use of the FarmData2 components.</p>
   <ul>
     <li>
-      <a href="active_plant_asset_picklist">ActivePlantAssetPicklist</a>: The
-      ActivePlantAssetPicklist allows the user to pick crops from a location.
+      <a href="active_plant_asset_picklist">ActivePlantAssetPicklist</a>: The ActivePlantAssetPicklist allows the user to pick crops from a location.
     </li>
     <li>
-      <a href="bed_picker">BedPicker</a>: The BedPicker component is a UI
-      element that allows the user to select beds from a within a location.
+      <a href="bed_picker">BedPicker</a>: The BedPicker component is a UI element that allows the user to select beds from a within a location.
     </li>
     <li>
-      <a href="comment_box">CommentBox</a>: The CommentBox component provides a
-      UI element that allows the user to enter a comment.
+      <a href="comment_box">CommentBox</a>: The CommentBox component provides a UI element that allows the user to enter a comment.
     </li>
     <li>
-      <a href="crop_selector">CropSelector</a>: The CropSelector component is a
-      UI element that allows the user to select a crop.
+      <a href="crop_selector">CropSelector</a>: The CropSelector component is a UI element that allows the user to select a crop.
     </li>
     <li>
-      <a href="date_selector">DateSelector</a>: The DateSelector component
-      provides a UI element for selecting a date.
+      <a href="date_selector">DateSelector</a>: The DateSelector component provides a UI element for selecting a date.
     </li>
     <li>
-      <a href="equipment_selector">EquipmentSelector</a>: The EquipmentSelector
-      is a component for selecting equipment that was used for an activity.
+      <a href="equipment_selector">EquipmentSelector</a>: The EquipmentSelector is a component for selecting equipment that was used for an activity.
     </li>
     <li>
-      <a href="location_selector">LocationSelector</a>: The LocationSelector
-      component provides a UI element that allows the user to select a location.
+      <a href="location_selector">LocationSelector</a>: The LocationSelector component provides a UI element that allows the user to select a location.
     </li>
     <li>
-      <a href="multi_crop_selector">MultiCropSelector</a>: The MultiCropSelector
-      is a component for selecting multiple crops.
+      <a href="multi_crop_selector">MultiCropSelector</a>: The MultiCropSelector is a component for selecting multiple crops.
     </li>
     <li>
-      <a href="multi_selector_base">MultiSelectorBase</a>: The MultiSelectorBase
-      is a component for selecting multiple items.
+      <a href="multi_selector_base">MultiSelectorBase</a>: The MultiSelectorBase is a component for selecting multiple items. 
     </li>
     <li>
-      <a href="numeric_input">NumericInput</a>: The NumericInput component is a
-      UI component used to read numeric input values from the user.
+      <a href="numeric_input">NumericInput</a>: The NumericInput component is a UI component used to read numeric input values from the user.
     </li>
     <li>
-      <a href="picker_base">PickerBase</a>: The PickerBase is a base component
-      that allows the user to pick multiple items using checkboxes.
+      <a href="picker_base">PickerBase</a>: The PickerBase is a base component that allows the user to pick multiple items using checkboxes.
     </li>
     <li>
-      <a href="picklist_base">PicklistBase</a>: The `PicklistBase` component
-      allows the user to pick multiple items from a list displayed as a table.
+      <a href="picklist_base">PicklistBase</a>: The `PicklistBase` component allows the user to pick multiple items from a list displayed as a table.
     </li>
     <li>
-      <a href="selector_base">SelectorBase</a>: The SelectorBase component is a
-      base component used to build other components
+      <a href="selector_base">SelectorBase</a>: The SelectorBase component is a base component used to build other components
     </li>
     <li>
-      <a href="soil_disturbance">SoilDisturbance</a>: The SoilDisturbance
-      component collects data on soil disturbances, including
+      <a href="soil_disturbance">SoilDisturbance</a>: The SoilDisturbance component collects data on soil disturbances, including
     </li>
     <li>
-      <a href="sort_order_button">SortOrderButton</a>: The SortOrderButton
-      component is used to toggle the sort order between ascending and
-      descending.
+      <a href="sort_order_button">SortOrderButton</a>: The SortOrderButton component is used to toggle the sort order between ascending and descending.
     </li>
     <li>
-      <a href="submit_reset_buttons">SubmitResetButtons</a>: The
-      SubmitResetButtons component provides the Submit and Reset buttons used on
-      forms.
+      <a href="submit_reset_buttons">SubmitResetButtons</a>: The SubmitResetButtons component provides the Submit and Reset buttons used on forms.
     </li>
     <li>
-      <a href="text_display">TextDisplay</a>: A TextDisplay component provides a
-      UI element for displaying a labeled text string.
+      <a href="text_display">TextDisplay</a>: A TextDisplay component provides a UI element for displaying a labeled text string.
     </li>
     <li>
-      <a href="transplanting_picklist">TransplantingPicklist</a>: A
-      TransplantingPicklist allows the user to select a crop from a list
+      <a href="transplanting_picklist">TransplantingPicklist</a>: A TransplantingPicklist allows the user to select a crop from a list
     </li>
     <li>
-      <a href="tray_size_selector">TraySizeSelector</a>: The TraySizeSelector
-      component is a UI element that allows the user to select a tray size (i.e.
-      number of cells) for a tray seeding.
+      <a href="tray_size_selector">TraySizeSelector</a>: The TraySizeSelector component is a UI element that allows the user to select a tray size (i.e. number of cells) for a tray seeding.
     </li>
     <li>
-      <a href="winter_kill">WinterKill</a>: The WinterKill component provides a
-      UI element for specifying if a crop seeding will be winter killed.
+      <a href="winter_kill">WinterKill</a>: The WinterKill component provides a UI element for specifying if a crop seeding will be winter killed.
     </li>
   </ul>
 
