2c6acefa046b232e61b921a70c73e34d684211d0
braughtg <braught@dickinson.edu>
2025-06-08 17:56:31 -0400

    patch script

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -69,7 +69,9 @@ const getModuleTestsE2ECyJs = (files) => {
   testCommands = files.map((file) => {
     if (file.includes('/farm_fd2/')) {
       // Skip this if the entrypoint has already been tested
-      if (!fd2EntrypointsTested.get(path.basename(path.dirname(file)))) {
+      if (fd2EntrypointsTested.get(path.basename(path.dirname(file)))) {
+        return 'stat --printf="Skipping %n" file';
+      } else {
         return (
           'test.bash --fd2 --e2e --live --glob=' +
           file.substring(file.indexOf('/modules'))
