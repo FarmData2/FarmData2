e49147b94cfb04e326b127caea12a6a1a3fcb3b4
braughtg <braught@dickinson.edu>
2025-07-01 15:55:39 -0400

    unit in formatter

--- modules/farm_fd2/src/module/Plugin/Field/FieldFormatter/FD2UnitConversionFormatter.php
+++ modules/farm_fd2/src/module/Plugin/Field/FieldFormatter/FD2UnitConversionFormatter.php
@@ -9,6 +9,7 @@ namespace Drupal\farm_fd2\Plugin\Field\FieldFormatter;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\Plugin\Field\FieldFormatter\EntityReferenceLabelFormatter;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\taxonomy\Entity\Term;
 
 /**
  * Plugin implementation of the 'fd2_unit_conversion_formatter' formatter.
@@ -31,70 +32,70 @@ class FD2UnitConversionFormatter extends EntityReferenceLabelFormatter {
   public static function defaultSettings() {
     return [
       'location' => 'suffix',
-      'template' => ' ({{ quantity }})',
+      'template' => ' ({{ quantity }} {{ unit }} = 1 {{ base_unit }})',
     ] + parent::defaultSettings();
   }
 
   /**
    * {@inheritdoc}
    */
-  public function settingsForm(array $form, FormStateInterface $form_state) {
-    $elements = parent::settingsForm($form, $form_state);
-    $elements['location'] = [
-      '#type' => 'radios',
-      '#options' => [
-        'pre-title' => t('Before the title'),
-        'post-title' => t('After the title'),
-        'suffix' => t('As part of the suffix'),
-        'attribute' => t('In a data attribute'),
-      ],
-      '#title' => t('Output location'),
-      '#default_value' => $this->getSetting('location'),
-      '#required' => TRUE,
-    ];
-    $elements['template'] = [
-      '#type' => 'textfield',
-      '#title' => t('Output template'),
-      '#default_value' => $this->getSetting('template'),
-      '#description' => t('A simple Twig snippet that outputs the "quantity" variable.'),
-      '#required' => TRUE,
-    ];
+  // public function settingsForm(array $form, FormStateInterface $form_state) {
+  //   $elements = parent::settingsForm($form, $form_state);
+  //   $elements['location'] = [
+  //     '#type' => 'radios',
+  //     '#options' => [
+  //       'pre-title' => t('Before the title'),
+  //       'post-title' => t('After the title'),
+  //       'suffix' => t('As part of the suffix'),
+  //       'attribute' => t('In a data attribute'),
+  //     ],
+  //     '#title' => t('Output location'),
+  //     '#default_value' => $this->getSetting('location'),
+  //     '#required' => TRUE,
+  //   ];
+  //   $elements['template'] = [
+  //     '#type' => 'textfield',
+  //     '#title' => t('Output template'),
+  //     '#default_value' => $this->getSetting('template'),
+  //     '#description' => t('A simple Twig snippet that outputs the "quantity" variable.'),
+  //     '#required' => TRUE,
+  //   ];
 
-    return $elements;
-  }
+  //   return $elements;
+  // }
 
   /**
    * {@inheritdoc}
    */
-  public function settingsSummary() {
-    $summary = parent::settingsSummary();
+  // public function settingsSummary() {
+  //   $summary = parent::settingsSummary();
 
-    switch ($this->getSetting('location')) {
-      case 'pre-title':
-        $location = $this->t('before the title');
-        break;
+  //   switch ($this->getSetting('location')) {
+  //     case 'pre-title':
+  //       $location = $this->t('before the title');
+  //       break;
 
-      case 'post-title':
-        $location = t('after the title');
-        break;
+  //     case 'post-title':
+  //       $location = t('after the title');
+  //       break;
 
-      case 'suffix':
-        $location = $this->t('as part of the suffix');
-        break;
+  //     case 'suffix':
+  //       $location = $this->t('as part of the suffix');
+  //       break;
 
-      case 'attribute':
-        $location = $this->t('in a data-* attribute');
-        break;
+  //     case 'attribute':
+  //       $location = $this->t('in a data-* attribute');
+  //       break;
 
-      default:
-        $location = $this->t('as part of the suffix');
-        break;
-    }
-    $summary[] = $this->t('Display @action', ['@action' => $location]);
-    $summary[] = $this->t('Display as: @template', ['@template' => $this->getSetting('template')]);
+  //     default:
+  //       $location = $this->t('as part of the suffix');
+  //       break;
+  //   }
+  //   $summary[] = $this->t('Display @action', ['@action' => $location]);
+  //   $summary[] = $this->t('Display as: @template', ['@template' => $this->getSetting('template')]);
 
-    return $summary;
-  }
+  //   return $summary;
+  // }
 
   /**
    * {@inheritdoc}
@@ -107,28 +108,38 @@ class FD2UnitConversionFormatter extends EntityReferenceLabelFormatter {
       if (!empty($values[$delta]['quantity'])) {
         /** @var \Drupal\Core\Template\TwigEnvironment $environment */
         $environment = \Drupal::service('twig');
-        $output = $environment->renderInline($this->getSetting('template'), ['quantity' => $values[$delta]['quantity']]);
 
-        switch ($this->getSetting('location')) {
-          case 'attribute':
-            $elements[$delta]['#attributes']['data-quantity'] = $output;
-            break;
+        $term_id = $values[$delta]['target_id'];
+        $unit_name = \Drupal\taxonomy\Entity\Term::load($term_id)->getName();
 
-          case 'pre-title':
-            $elements[$delta]['#title'] .= $output;
-            break;
+        $output = $environment->renderInline(
+          $this->getSetting('template'), [
+            'quantity' => $values[$delta]['quantity'],
+            'unit' => $unit_name,
+            'base_unit' => "shit! 4"
+          ]
+        );
 
-          case 'post-title':
-            $elements[$delta]['#title'] = $output . $elements[$delta]['#title'];
-            break;
+    //     switch ($this->getSetting('location')) {
+    //       case 'attribute':
+    //         $elements[$delta]['#attributes']['data-quantity'] = $output;
+    //         break;
 
-          case 'suffix':
-            if (!isset($elements[$delta]['#suffix'])) {
-              $elements[$delta]['#suffix'] = '';
-            }
-            $elements[$delta]['#suffix'] = $output;
-            break;
-        }
+    //       case 'pre-title':
+    //         $elements[$delta]['#title'] .= $output;
+    //         break;
+
+    //       case 'post-title':
+    //         $elements[$delta]['#title'] = $output . $elements[$delta]['#title'];
+    //         break;
+
+    //       case 'suffix':
+    //         if (!isset($elements[$delta]['#suffix'])) {
+    //           $elements[$delta]['#suffix'] = '';
+    //         }
+             $elements[$delta]['#suffix'] = $output;
+    //         break;
+    //     }
       }
     }
 
