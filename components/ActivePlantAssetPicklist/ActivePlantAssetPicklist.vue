aec6700053afddcb41634eca753f3b9712b8d6d0
Fej Esievo <116123894+fejesievo@users.noreply.github.com>
2025-07-11 12:00:45 -0400

    Required row update (#515)
    **Pull Request Description**
    
    Updates eactive plant asset picklist example page so that required is
    forced to be true when requiredRow is true.
    
    
    Closes #489 
    ---
    
    **Licensing Certification**
    
    FarmData2 is a [Free Cultural
    Work](https://freedomdefined.org/Definition) and all accepted
    contributions are licensed as described in the LICENSE.md file. This
    requires that the contributor holds the rights to do so. By submitting
    this pull request **I certify that I satisfy the terms of the [Developer
    Certificate of Origin](https://developercertificate.org/)** for its
    contents.
    
    ---------
    
    Co-authored-by: braughtg <braught@dickinson.edu>

--- components/ActivePlantAssetPicklist/ActivePlantAssetPicklist.vue
+++ components/ActivePlantAssetPicklist/ActivePlantAssetPicklist.vue
@@ -118,14 +118,13 @@ export default {
     },
     /**
      * If true, enforce at least one row in the PicklistBase.
-     * Ignored if `required` is true.
      */
     requiredRow: {
       type: Boolean,
       default: false,
     },
     /**
-     * Whether at least one crop must be picked or not.
+     * Whether at least one crop or bed must be picked.
      */
     required: {
       type: Boolean,
