36d36e22f030e3bd0c0b6e5c99936d4813210fe5
Grant Braught <braught@dickinson.edu>
2026-02-03 20:13:43 +0000

    messing around again ;{

--- .devcontainer/postAttach.bash
+++ .devcontainer/postAttach.bash
@@ -3,8 +3,12 @@
 # This runs each time the project folder is opened in VSCode.
 # E.g. on a browser window reload or opening a new window to a running container.
 
-
 source bin/lib/checkServers.lib.bash
+
+# Print out links here so that VSCode picks them up and exposes the ports.
+PROXY_PORT=$(docker port fd2_nginx | cut -d':' -f2 | head -1)
+echo "https proxy is using: https://localhost:${PROXY_PORT}"
+echo "noVNC is using: http://localhost:6901"
 
 # Wait until postgres, noVNC, farmOS and nginx have started and 
 # display a message for the user.
@@ -24,3 +28,4 @@ else
   echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
   echo ""
 fi
+
