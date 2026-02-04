76dd85ec3e0ab7755fdc9d0cdda37f4e3c3496c6
Grant Braught <braught@dickinson.edu>
2026-02-04 12:57:40 +0000

    fix stuff that didn't get commited

--- .devcontainer/postStart.bash
+++ .devcontainer/postStart.bash
@@ -6,6 +6,6 @@
 # The group on docker.sock is not preserved across 
 # devcontainer restarts so set it here so it happens
 # every time a new container is created.
-echo "Setting group for docker.sock..."
-sudo chgrp docker /var/run/docker.sock
-echo "Group set."
+# echo "Setting group for docker.sock..."
+# sudo chgrp docker /var/run/docker.sock
+# echo "Group set."
