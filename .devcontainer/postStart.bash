5f609c9e2c389615b9af22630bf5287529162e3f
Grant Braught <braught@dickinson.edu>
2026-02-04 22:19:29 +0000

    clean up docker dir

--- .devcontainer/postStart.bash
+++ .devcontainer/postStart.bash
@@ -3,9 +3,3 @@
 # This runs every time the dev container starts.
 # It makes non-persistent changes.
 
-# The group on docker.sock is not preserved across 
-# devcontainer restarts so set it here so it happens
-# every time a new container is created.
-# echo "Setting group for docker.sock..."
-# sudo chgrp docker /var/run/docker.sock
-# echo "Group set."
