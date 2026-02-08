ae08b19fae100971e11d805866b696bb50676419
Grant Braught <braught@dickinson.edu>
2026-02-08 14:05:17 +0000

    remove chown sincef fd2dev is now default user

--- .devcontainer/postCreate.bash
+++ .devcontainer/postCreate.bash
@@ -4,7 +4,7 @@
 # This installs npm dependencies and builds the modules and docs.
 
 # Reassign all of the workshpace files to the fd2dev user.
-sudo chown -R fd2dev:fd2dev /workspaces
+#sudo chown -R fd2dev:fd2dev /workspaces
 
 REPO_DIR=$(git rev-parse --show-toplevel)
 source "$REPO_DIR/bin/lib/checkServices.lib.bash"
